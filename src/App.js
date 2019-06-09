58461667ffa08f1158a4c8d8f6e3a2aa4619de77
Zac Bell <zacdev@outlook.com>
2019-06-09 15:47:14 -0500

    autoformat on save, add Tilt to images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
-import { CSSTransition } from 'react-transition-group';
+import Tilt from "react-tilt";
+import { CSSTransition } from "react-transition-group";
 import PixoulContactForm from "./components/ContactForm/index";
 import "./App.css";
 import bgImage from "./images/bg.png";
@@ -20,24 +21,24 @@ import artisanLogo from "./images/artisanLogo.png";
 class App extends React.Component {
   state = {
     page: 0,
-    showMenu: false,
+    showMenu: false
   };
 
-  setShowMenu = (showMenu) => {
+  setShowMenu = showMenu => {
     this.setState({ showMenu });
-  }
+  };
 
   onLeave = (origin, destination, direction) => {
     this.setState({ page: destination.index });
-  }
+  };
 
   onMenuButtonClicked = () => {
     this.setShowMenu(!this.state.showMenu);
-  }
+  };
 
   render() {
     const { showMenu } = this.state;
-    const pageIndex = ['00', '01', '02', '03', '04', '05', '06', 'Contact'];
+    const pageIndex = ["00", "01", "02", "03", "04", "05", "06", "Contact"];
     return (
       <CSSTransition
         in={!showMenu}
@@ -46,9 +47,12 @@ class App extends React.Component {
         onEnter={() => {}}
         onExited={() => {}}
       >
-        <div className={showMenu ? 'main__exited' : ''}>
+        <div className={showMenu ? "main__exited" : ""}>
           <Header onMenuPressed={this.onMenuButtonClicked} />
-          <PageIndex style={{ right: 44, bottom: 67 }} selected={pageIndex[this.state.page]} />
+          <PageIndex
+            style={{ right: 44, bottom: 67 }}
+            selected={pageIndex[this.state.page]}
+          />
           <ReactFullpage
             onLeave={this.onLeave}
             render={({ state, fullpageApi }) => {
@@ -65,9 +69,9 @@ class App extends React.Component {
                           <div className="blue_line_under_title" />
                         </div>
                         <div className="first-description">
-                          We’re a UX design agency, full-stack development shop, and
-                          strategic consulting group—ready to bring your digital
-                          vision to life.
+                          We’re a UX design agency, full-stack development shop,
+                          and strategic consulting group—ready to bring your
+                          digital vision to life.
                         </div>
                       </div>
                       <button
@@ -88,8 +92,22 @@ class App extends React.Component {
                         is redefining the business marketplace.
                       </span>
                     </div>
-                    <img src={bg01} alt="bg01" id="bg01" className="bg-img" />
-                    <LearnMore style={{ bottom: '148px', left: '361px' }} />
+                    <Tilt
+                      className="Tilt"
+                      options={{
+                        max: 15,
+                        perspective: 8000,
+                        scale: 1
+                      }}
+                    >
+                      <img
+                        src={bg01}
+                        alt="bg01"
+                        id="bg01"
+                        className="Tilt-inner bg-img"
+                      />
+                    </Tilt>
+                    <LearnMore style={{ bottom: "148px", left: "361px" }} />
                     <span className="watermark">01</span>
                   </div>
                   <div className="section">
@@ -102,8 +120,22 @@ class App extends React.Component {
                         is thinking smarter about how we power the world.
                       </span>
                     </div>
-                    <img src={bg02} alt="bg02" id="bg02" className="bg-img" />
-                    <LearnMore style={{ bottom: '148px', left: '361px' }} />
+                    <Tilt
+                      className="Tilt"
+                      options={{
+                        max: 15,
+                        perspective: 8000,
+                        scale: 1
+                      }}
+                    >
+                      <img
+                        src={bg02}
+                        alt="bg02"
+                        id="bg02"
+                        className="Tilt-inner bg-img"
+                      />
+                    </Tilt>
+                    <LearnMore style={{ bottom: "148px", left: "361px" }} />
                     <span className="watermark">02</span>
                   </div>
                   <div className="section">
@@ -114,8 +146,22 @@ class App extends React.Component {
                         is setting the standard in healthcare—literally.
                       </span>
                     </div>
-                    <img src={bg03} alt="bg03" id="bg03" className="bg-img" />
-                    <LearnMore style={{ bottom: '148px', left: '361px' }} />
+                    <Tilt
+                      className="Tilt"
+                      options={{
+                        max: 15,
+                        perspective: 8000,
+                        scale: 1
+                      }}
+                    >
+                      <img
+                        src={bg03}
+                        alt="bg03"
+                        id="bg03"
+                        className="Tilt-inner bg-img"
+                      />
+                    </Tilt>
+                    <LearnMore style={{ bottom: "148px", left: "361px" }} />
                     <span className="watermark">03</span>
                   </div>
                   <div className="section">
@@ -127,8 +173,22 @@ class App extends React.Component {
                         is financing the future of personal banking loans.
                       </span>
                     </div>
-                    <img src={bg04} alt="bg04" id="bg04" className="bg-img" />
-                    <LearnMore style={{ bottom: '148px', left: '361px' }} />
+                    <Tilt
+                      className="Tilt"
+                      options={{
+                        max: 15,
+                        perspective: 8000,
+                        scale: 1
+                      }}
+                    >
+                      <img
+                        src={bg04}
+                        alt="bg04"
+                        id="bg04"
+                        className="Tilt-inner bg-img"
+                      />
+                    </Tilt>
+                    <LearnMore style={{ bottom: "148px", left: "361px" }} />
                     <span className="watermark">04</span>
                   </div>
                   <div className="section">
@@ -139,8 +199,22 @@ class App extends React.Component {
                         is reworking your wardrobe, one piece at a time.
                       </span>
                     </div>
-                    <img src={bg05} alt="bg05" id="bg05" className="bg-img" />
-                    <LearnMore style={{ bottom: '148px', left: '361px' }} />
+                    <Tilt
+                      className="Tilt"
+                      options={{
+                        max: 15,
+                        perspective: 8000,
+                        scale: 1
+                      }}
+                    >
+                      <img
+                        src={bg05}
+                        alt="bg05"
+                        id="bg05"
+                        className="Tilt-inner bg-img"
+                      />
+                    </Tilt>
+                    <LearnMore style={{ bottom: "148px", left: "361px" }} />
                     <span className="watermark">05</span>
                   </div>
                   <div className="section">
@@ -153,8 +227,22 @@ class App extends React.Component {
                         is making a treat of wholesale subscriptions.
                       </span>
                     </div>
-                    <img src={bg06} alt="bg06" id="bg06" className="bg-img" />
-                    <LearnMore style={{ bottom: '148px', left: '361px' }} />
+                    <Tilt
+                      className="Tilt"
+                      options={{
+                        max: 15,
+                        perspective: 8000,
+                        scale: 1
+                      }}
+                    >
+                      <img
+                        src={bg06}
+                        alt="bg06"
+                        id="bg06"
+                        className="Tilt-inner bg-img"
+                      />
+                    </Tilt>
+                    <LearnMore style={{ bottom: "148px", left: "361px" }} />
                     <span className="watermark">06</span>
                   </div>
                   <div className="section">
@@ -169,6 +257,5 @@ class App extends React.Component {
     );
   }
 }
-
 
 export default App;
