c419282d1b2107698e8dfc75e31281d6215f8e61
Zac Bell <zacdev@outlook.com>
2019-06-09 02:39:09 -0400

    updated stylesheets & minor issues

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -21,52 +21,52 @@ class PixoulContactForm extends Component {
             alt="Contact Form Envelope"
           />
         </div>
-        <div class="contact_section_form_container">
-          <form class="contact_section_form">
-            <div class="group">
+        <div className="contact_section_form_container">
+          <form className="contact_section_form">
+            <div className="group">
               <input
                 type="text"
                 id="contact_form_first_name"
                 required="required"
               />
               <label for="contact_form_first_name">
-                First Name <span class="asterix">*</span>
+                First Name <span className="asterix">*</span>
               </label>
             </div>
-            <div class="group">
+            <div className="group">
               <input
                 type="text"
                 id="contact_form_last_name"
                 required="required"
               />
               <label for="contact_form_last_name">
-                Last Name <span class="asterix">*</span>
+                Last Name <span className="asterix">*</span>
               </label>
             </div>
-            <div class="group">
+            <div className="group">
               <input type="text" id="contact_form_email" required="required" />
               <label for="contact_form_email">
-                Email <span class="asterix">*</span>
+                Email <span className="asterix">*</span>
               </label>
             </div>
-            <div class="group">
+            <div className="group">
               <input type="text" id="contact_form_phone" required="required" />
               <label for="contact_form_phone">
-                Phone <span class="asterix">*</span>
+                Phone <span className="asterix">*</span>
               </label>
             </div>
-            <div class="group group_last_child">
+            <div className="group group_last_child">
               <input
                 type="text"
                 id="contact_form_interest"
                 required="required"
               />
               <label for="contact_form_interest">
-                I'm Interested In...<span class="asterix">*</span>
+                I'm Interested In...<span className="asterix">*</span>
               </label>
             </div>
-            <div class="contact_form_submit_btn_container">
-              <button class="contact_form_submit_btn" value="submit">
+            <div className="contact_form_submit_btn_container">
+              <button className="contact_form_submit_btn" value="submit">
                 Send
               </button>
             </div>
