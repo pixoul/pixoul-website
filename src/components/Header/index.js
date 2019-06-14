5efd0a6993c9df8c779e9c7e97c5ba751f64fa13
Zac Bell <zacdev@outlook.com>
2019-06-13 21:25:48 -0400

    fixed the text scramble on header menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Scramble from 'react-scramble'
 import logo from "../../images/logo.svg";
 import HamburgerMenuImg from "../../images/HamburgerMenu.png";
 import "./header.css";
@@ -7,7 +8,23 @@ const Header = props => {
   return (
     <div className="header">
       <img className="logo" src={logo} alt="logoTitle" />
-      <button className="pixoul_button contactus-button">Contact Us</button>
+      <button className="pixoul_button contactus-button">
+        <Scramble
+          text="Contact Us"
+          mouseEnterTrigger="restart"
+          steps={[
+            {
+              roll: 3,
+              action: '+',
+              type: 'all',
+            },
+            {
+              action: '-',
+              type: 'forward',
+            },
+          ]}
+        />
+      </button>
       <button
         className="pixoul_button hamburger-button"
         onClick={props.onMenuPressed}
