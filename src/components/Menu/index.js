415c7714fa45ccdfdac4aff7dfe579fd3dc0d336
Zac Bell <zacdev@outlook.com>
2019-06-17 08:39:48 -0400

    added menu bar

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,12 +5,24 @@ import "./menu.css";
 
 const Menu = props => {
   return (
-    <div className="menu">
-      <Link className="menu-item" to='/'>Home</Link>
-      <Link className="menu-item" to='/work'>Work</Link>
-      <Link className="menu-item" to='/aboutus'>About</Link>
-      <Link className="menu-item" to='/services'>Services</Link>
-      <Link className="menu-item" to='/Blog'>Blog</Link>
+    <div className="menu-container">
+      <div className="menu">
+        <Link className="menu-item" to='/'>Home</Link>
+        <Link className="menu-item" to='/work'>Work</Link>
+        <Link className="menu-item" to='/aboutus'>About</Link>
+        <Link className="menu-item" to='/services'>Services</Link>
+        <Link className="menu-item" to='/blog'>Blog</Link>
+        <div className="menu-contact-section">
+          <p>(616) 914-9356</p>
+          <p>hello@pixoulagency.com</p>
+        </div>
+        <a className="menu-contact-button" href="#Contact">Contact Us</a>
+        
+        <div className="menu-link-section">
+          <div>&copy; 2019 Pixoul. All rights reserved.</div>
+          <Link className="small-menu-item" to='/privacy-policy'>Privacy Policy</Link> | <Link className="small-menu-item" to='/tos'>Terms of Service</Link>
+        </div>
+      </div>
     </div>
   );
 };
