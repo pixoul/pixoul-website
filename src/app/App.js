26b910bc7ccb65c43b87274367ee6a42de9db30c
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-18 16:29:53 -0500

    chore: cleansup and removes all content to start fresh

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { CSSTransition } from "react-transition-group";
+import { Route } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
