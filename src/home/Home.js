c7b2d14218b6e6a77182a7b2edaed1ef8c96ccae
Ryan Thompson <ryan@ryanthompson.io>
2019-07-20 13:23:07 -0500

    cleanup: updates the Header and Home page to use new Hero component and removes Intro component

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-import Intro from "home/Intro"
+import Header from "app/Header"
+import Hero from "common/Hero/Hero"
+import bg from "images/bg.png"
+import favicon from "images/favicon.svg"
+import arrow from "images/arrow_down.svg"
 
 export default function Home() {
   return(
     <div>
-      <Intro />
+      <Hero
+        bg={bg}
+        header={<Header />}
+        title={<img src={favicon} />}
+        tagline="We’re a digital services firm with a focus on design, technology, and strategic innovation."
+        action={<img src={arrow} />}
+        footer="See how we help ambitious brands dominate digital."
+      />
     </div>
   )
 }
