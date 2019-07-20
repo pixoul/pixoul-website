21f344d66bf372db729b15f1ba5e87a126a51771
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-20 13:46:27 -0500

    feature: adds basic SideMenu component

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "app/Header"
+import SideMenu from "app/SideMenu"
 import Home from "home/Home"
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <SideMenu />
         <Home />
       </div>
     );
