e2dfc0512012985962a04dbe7e0e97fe8b3c6844
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-20 13:23:07 -0500

    cleanup: updates the Header and Home page to use new Hero component and removes Intro component

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from "shards-react";
 
-import logo from "images/logo.svg"
+import logo from "images/logo.png"
+import menu from "images/menu.svg"
 
 export default function Header() {
   // Declare a new state variable, which we'll call "count"
   // const [count, setCount] = useState(0);
 
   return (
-    <Navbar type="light" expand="md">
+    <Navbar type="light" expand="md" theme="transparent">
       <NavbarBrand href="#"><img src={logo} /></NavbarBrand>
-      <Nav className="ml-auto">
+      <Nav className="ml-auto align-items-center">
         <NavItem>
-          <Button outline>Contact Us</Button>
+          <Button outline >Contact Us</Button>
         </NavItem>
         <NavItem>
-
+          <NavLink href="#" ><img src={menu} /></NavLink>
         </NavItem>
       </Nav>
     </Navbar>
