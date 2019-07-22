6b9d71e9d2d236f5d71b07c9ecb9b30b6a714ce5
Ryan Thompson <ryan@ryanthompson.io>
2019-07-22 14:10:28 -0500

    chore: imports avenir next font and removes roboto

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Route, BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
-import "typeface-roboto";
+import "styles/fonts.scss";
 
 const routing = (
   <ParallaxProvider>
