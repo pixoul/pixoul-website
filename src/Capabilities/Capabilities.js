6d478191b86fe73fe942edc073d07caec99f3b4c
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-22 16:35:36 -0500

    feature: creates feature collection for home page and capabilities page

--- src/Capabilities/Capabilities.js
+++ src/Capabilities/Capabilities.js
@@ -1,7 +1,26 @@
 import React from "react"
 
+import FeatureCollection from "Capabilities/FeatureCollection/FeatureCollection"
+import Hero from "Utils/Hero/Hero"
+
+import bg from "images/bg07.png"
+import arrow from "images/arrow_down.svg"
+import phone from "images/phone-mockup.png"
+
 export default function Capabilities(){
   return(
-    <div></div>
+    <div>
+      <Hero
+        bg={bg}
+        image={<img src={phone} />}
+        title="How can we help?"
+        tagline="We’re here to bring life to your digital vision. Got a project in mind? Let’s talk."
+        action={<img src={arrow} />}
+        theme="light"
+        align="left"
+      />
+
+      <FeatureCollection />
+    </div>
   )
 }
