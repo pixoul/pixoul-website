60089da9ddb456d3919978e11e620dc2f6e89089
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-22 19:32:38 -0500

    cleanup: styling cleanup of the hero component across all three pages

--- src/about/About.js
+++ src/about/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// import Hero from "Utils/Hero/Hero"
+import { Hero, HeroContent, HeroFooter, HeroMedia, HeroTitle, HeroAction, HeroTagline} from "Utils/Hero/Hero"
 import bg from "images/bg08.png"
 import favicon from "images/favicon.svg"
 import arrow from "images/arrow_down.svg"
@@ -9,7 +9,23 @@ import browser from "images/browser_window.svg"
 export default function About() {
   return(
     <div>
-
+    <Hero bg={bg} theme="dark" align="center">
+      <HeroContent>
+        <HeroTitle
+          text="Complex problems deserve elegant solutions."
+          width={874}
+          size={65}
+        />
+        <HeroTagline
+          text="We help brands and organizations succeed in the digital age."
+          size={22}
+        />
+        <HeroAction />
+      </HeroContent>
+      <HeroFooter>
+        <HeroMedia media={browser} />
+      </HeroFooter>
+    </Hero>
 
     </div>
   )
