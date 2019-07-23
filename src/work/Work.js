14c4a944bc421ab15f490e67d7b79ccaf74d8627
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-23 18:21:44 -0500

    refractor: moves iuzeit to its own component for better flexibility

--- src/work/Work.js
+++ src/work/Work.js
@@ -1,20 +1,12 @@
 import React from "react"
 
-import ClientSummary from "utils/client-summary/client-summary"
-
-import bg from "./iuzeit/bg01.png"
-import logo from "./iuzeit/iuzeit.svg"
+import Iuzeit from "work/iuzeit"
 
 export default function Work(){
   return(
     <div>
 
-      <ClientSummary
-        logo={logo}
-        action="/work/detail/iuzeit"
-        media={bg}
-        tagline="is shapping the future of the online purchase journey"
-      />
+      <Iuzeit />
     </div>
   )
 }
