c7d44cad849fa8fb4c7b8c37f82086ab2bb891c4
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-24 12:12:42 -0500

    refractor: splits feature component out into subcomponents for better flow control

--- src/capabilities/FeatureCollection/FeatureCollection.js
+++ src/capabilities/FeatureCollection/FeatureCollection.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "shards-react";
 import { faPowerOff, faPaintBrush, faCode } from '@fortawesome/free-solid-svg-icons'
 import "./FeatureCollection.scss"
 
-import Feature from "utils/Feature/Feature"
+import { Feature, FeatureHeader, FeatureTitle, FeatureIcon, FeatureTagline, FeatureAction } from "utils/feature/feature"
 import Opening from "utils/opening/opening"
 
 export default function FeatureCollection(){
@@ -15,31 +15,34 @@ export default function FeatureCollection(){
       />
       <Row>
         <Col sm="12" md="4">
-          <Feature
-            title="Strategy"
-            tagline="Reimagine and redefine your organizational approach by developing new ways of attaining mission-critical KPIs."
-            actionLink="/capabilities/strategy"
-            actionText="Learn More"
-            icon={faPowerOff}
-          />
+          <Feature>
+            <FeatureHeader>
+              <FeatureIcon icon={faPowerOff} />
+              <FeatureTitle text="Strategy" />
+            </FeatureHeader>
+            <FeatureTagline text="Reimagine and redefine your organizational approach by developing new ways of attaining mission-critical KPIs." />
+            <FeatureAction link="/capabilities/strategy" text="Learn More" />
+          </Feature>
         </Col>
         <Col sm="12" md="4">
-          <Feature
-            title="Design Thinking"
-            tagline="Use Design Thinking to create products, services, systems, and experiences that address core needs of your target users."
-            actionLink="/capabilities/design"
-            actionText="Learn More"
-            icon={faPaintBrush}
-          />
+          <Feature>
+            <FeatureHeader>
+              <FeatureIcon icon={faPaintBrush} />
+              <FeatureTitle text="Design Thinking" />
+            </FeatureHeader>
+            <FeatureTagline text="Use Design Thinking to create products, services, systems, and experiences that address core needs of your target users." />
+            <FeatureAction link="/capabilities/design" text="Learn More" />
+          </Feature>
         </Col>
         <Col sm="12" md="4">
-          <Feature
-            title="Technology"
-            tagline="Bring your digital solution to life with cutting-edge infrastructure that can scale across devices and platforms."
-            actionLink="/capabilities/technology"
-            actionText="Learn More"
-            icon={faCode}
-          />
+          <Feature>
+            <FeatureHeader>
+              <FeatureIcon icon={faCode} />
+              <FeatureTitle text="Technology" />
+            </FeatureHeader>
+            <FeatureTagline text="Bring your digital solution to life with cutting-edge infrastructure that can scale across devices and platforms." />
+            <FeatureAction link="/capabilities/technology" text="Learn More" />
+          </Feature>
         </Col>
       </Row>
     </Container>
