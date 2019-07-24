28cc11adf54720e2e33dad442a5d6670d3cd4370
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-24 17:09:35 -0500

    feature: adds the capability subcomponents to the routes

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./app.scss"
 
 import Header from "layout/header/header"
--- src/capabilities/technology/technology.js
+++ src/capabilities/technology/technology.js
@@ -1,4 +1,4 @@
-import Reach from "react"
+import React from "react"
 
 export default function Technology(){
   return(
