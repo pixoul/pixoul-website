1f025130898505a679b247f85532a29dbe22b94f
Ryan Thompson <ryan@ryanthompson.io>
2019-07-24 13:40:26 -0500

    fix: enhances opening component spacing

--- src/utils/opening/opening.js
+++ src/utils/opening/opening.js
@@ -2,8 +2,14 @@ import React from "react"
 import "./opening.scss"
 
 export default function Opening(props){
+
+  const styles = {
+    width: props.width ? props.width : 800
+  }
+
+
   return(
-    <div className="opening">
+    <div className="opening" style={styles}>
       <h3 className="opening-title">{props.title}</h3>
       <p className="opening-subtitle">{props.subtitle}</p>
     </div>
