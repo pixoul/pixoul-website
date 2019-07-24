619930311394f2a448f3833df4442f707460336e
Ryan Thompson <ryan@ryanthompson.io>
2019-07-24 12:12:42 -0500

    refractor: splits feature component out into subcomponents for better flow control

--- src/utils/Feature/Feature.js
+++ src/utils/Feature/Feature.js
@@ -2,23 +2,61 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfo, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
-import "./Feature.scss"
+import "./feature.scss"
+
+
+function FeatureTagline(props){
+  return(
+    <small className="feature-tagline">{props.text}</small>
+  )
+}
+
+function FeatureAction(props){
+  return(
+    <Link to={props.link} className="feature-action">
+      <small>
+        <strong>{props.text} <FontAwesomeIcon icon={faLongArrowAltRight} /></strong>
+      </small>
+    </Link>
+  )
+}
+
+function FeatureTitle(props){
+  return(
+    <h6 className="feature-title">{props.text}</h6>
+  )
+}
+
+function FeatureIcon(props){
+  return(
+    <FontAwesomeIcon icon={props.icon} className="feature-icon" />
+  )
+}
+
+function FeatureHeader(props){
+  return(
+    <div className="feature-media">
+      {props.children}
+    </div>
+  )
+}
 
 export default function Feature(props){
   const { title = "Feature", tagline = "This is a feature.", actionLink = "#", actionText = "Learn More", icon = faInfo} = props
 
   return(
-    <div className="shards-feature">
-      <div className="feature-media">
-        <FontAwesomeIcon icon={icon} className="feature-icon" />
-        <h6 className="feature-title">{title}</h6>
-      </div>
-      <small className="feature-tagline">{tagline}</small>
-      <Link to={actionLink} className="feature-action">
-        <small>
-          <strong>{actionText} <FontAwesomeIcon icon={faLongArrowAltRight} /></strong>
-        </small>
-      </Link>
+    <div className="feature">
+      {props.children}
     </div>
   )
+}
+
+
+export {
+  Feature,
+  FeatureHeader,
+  FeatureTitle,
+  FeatureIcon,
+  FeatureTagline,
+  FeatureAction
 }
