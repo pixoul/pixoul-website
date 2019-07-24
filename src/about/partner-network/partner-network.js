bbd1b20b9c5888cc36dd95619bfe275a419a64f6
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-24 14:44:39 -0500

    feature: implements parallel display in partner network component

---
+++ src/about/partner-network/partner-network.js
@@ -0,0 +1,55 @@
+import React from "react"
+import "./partner-network.scss"
+
+import Opening from "utils/opening/opening"
+import {
+  ParallelDisplay,
+  ParallelContent,
+  ParallelList,
+  ParallelItem,
+  ParallelImage,
+  ParallelText
+} from "utils/parallel-display/parallel-display"
+
+import map from "images/bitmap.jpg"
+
+export default function PartnerNetwork(){
+  return(
+    <div className="partner-network">
+
+      <ParallelDisplay>
+        <ParallelContent>
+          <ParallelImage media={map} width={606} height={563} />
+        </ParallelContent>
+
+
+        <ParallelContent>
+          <ParallelText
+            width={560}
+            size={45}
+            color="#000000"
+            text="Our partner network is always expanding."
+          />
+          <ParallelText
+            width={530}
+            size={20}
+            text="Headquartered in Dallas, TX, we’re proud to have made an impact on communities around the world."
+          />
+
+          <ParallelList>
+            <ParallelItem
+              title="Team Members"
+              tagline="Across North America."
+            />
+
+            <ParallelItem
+              title="Clients"
+              tagline="Around the globe."
+            />
+          </ParallelList>
+        </ParallelContent>
+      </ParallelDisplay>
+
+    </div>
+  )
+}
