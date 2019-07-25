7091ee44870391fc54f0e142914dbf08b93d74a2
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-24 20:10:26 -0500

    feature: create review card component

---
+++ src/utils/review-card/review-card.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+import "./review-card.scss"
+
+export default function ReviewCard({
+  quantity = 0,
+  review,
+  avatar,
+  reviewer,
+  designation
+}){
+  return(
+    <div className="review-card">
+      <div className="review-stars">
+        {[...Array(quantity)].map((e, i) => <FontAwesomeIcon icon={faStar} className="star-icon" />)}
+      </div>
+      <div className="review-content">
+        <p className="review-text">{review}</p>
+        <img src={avatar} className="reviewer-avatar"/>
+        <h4 className="reviewer-name">{reviewer}</h4>
+        <h6 className="reviewer-designation">{designation}</h6>
+      </div>
+    </div>
+  )
+}
