c685c6d9837aa2c2bd46b1d74e6ac5891ebb7dea
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-25 10:29:38 -0500

    fix: updates feature and capabilities collection to use pro font-awesome icons

--- src/utils/feature/feature.js
+++ src/utils/feature/feature.js
@@ -2,7 +2,7 @@ import React from "react"
 import cn from "classnames"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import { faLongArrowRight } from '@fortawesome/pro-light-svg-icons'
 import "./feature.scss"
 
 
@@ -27,7 +27,7 @@ function FeatureAction(props){
 
   return(
     <Link to={props.link} className="feature-action" style={styles}>
-        {props.text} <FontAwesomeIcon icon={faLongArrowAltRight} />
+        {props.text} <FontAwesomeIcon icon={faLongArrowRight} />
     </Link>
   )
 }
