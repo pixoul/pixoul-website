12a797df968bf3ab7f0dba49fcd261f05a1796ea
Ryan Thompson <ryan@ryanthompson.io>
2019-07-25 11:20:13 -0500

    feature: creates technology capabilities page and creates starter technology list component

---
+++ src/capabilities/technology/technology-list/technology-list.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./technology-list.scss"
+
+import adobe from "./images/adobe.svg"
+import android from "./images/android.svg"
+import angular from "./images/angular.svg"
+import apple from "./images/apple.svg"
+import atom from "./images/atom.svg"
+import magento from "./images/magento.svg"
+import shopify from "./images/shopify.svg"
+import vue from "./images/vue.svg"
+
+export default function TechnologyList(){
+  return(
+    <div>
+
+    </div>
+  )
+}
