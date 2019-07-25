2dfffdb828b08e01e70ea58835768bbdc9c46ae3
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-25 17:50:37 -0500

    feature: create contact form (no function) and aligns with design

---
+++ src/contact/contact-form/contact-form.js
@@ -0,0 +1,58 @@
+import React from "react"
+import "./contact-form.scss"
+import { Container, Row, Col, Form, FormInput, FormGroup, FormTextarea, Button } from "shards-react";
+
+export default function ContactForm(){
+
+  // const onChange = (item, event) => {
+  //   this.setState({ [item]: event.target.value });
+  // }
+  //
+  // const sendEmail = e => {
+  //  e.preventDefault();
+  //  const { firstName, lastName, phone, email, interestedIn } = this.state;
+  //  const service_id = "default_service";
+  //  const template_id = "template_3l1ZvrRp";
+  //  emailjs.send(service_id,
+  //   template_id, {
+  //     "email": email,
+  //     "first_name": firstName,
+  //     "last_name": lastName,
+  //     "phone": phone,
+  //     "interestedIn": interestedIn,
+  //   },
+  //   'user_y5b1msGPAYKIW4szoHygG')
+  //   .then((response) => {
+  //       alert('Email sent!');
+  //   }, (err) => {
+  //       alert('Failed, please try again later');
+  //   });
+  // }
+
+
+  return(
+    <div className="contact-form">
+      <Form className="form">
+        <Row>
+          <Col>
+            <FormGroup>
+              <label htmlFor="#name">Name</label>
+              <FormInput id="#name" placeholder="Name" />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <label htmlFor="#email">Email</label>
+              <FormInput type="email" id="#email" placeholder="Email" />
+            </FormGroup>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm="12"><FormTextarea className="form-message" /></Col>
+          <Col sm="12"><Button block className="form-button">Send Message</Button></Col>
+        </Row>
+      </Form>
+    </div>
+  )
+}
--- src/contact/contact.js
+++ src/contact/contact.js
@@ -2,51 +2,32 @@ import React from "react";
 import { Container, Row, Col, Form, FormInput, FormGroup, FormTextarea, Button } from "shards-react";
 import "./contact.scss"
 
+import Opening from "utils/opening/opening"
+import ContactForm from "contact/contact-form/contact-form"
+
 export default function Contact() {
   return(
-    <Container className="contact">
-      <Row>
-        <Col>
-          <h3>Contact Us</h3>
-          <p>See how we help ambitious brands use design thinking to dominate digital.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <ul className="list-collection">
-            <li className="list-title">Phone</li>
-            <li className="list-item"><a >(616) 914-9356</a></li>
-          </ul>
+    <div className="contact">
+      <Container>
+        <Opening
+          title="Contact Us"
+          subtitle="See how we help ambitious brands use design thinking to dominate digital."
+        />
 
-          <ul className="list-collection">
-            <li className="list-title">Email</li>
-            <li className="list-item"><a >hello@pixoulinc.com</a></li>
-          </ul>
-        </Col>
-        <Col>
-          <Form>
-            <Row>
-              <Col>
-                <FormGroup>
-                  <label htmlFor="#full-name">Full Name</label>
-                  <FormInput id="#full-name" placeholder="Full Name" />
-                </FormGroup>
-              </Col>
-              <Col>
-                <FormGroup>
-                  <label htmlFor="#email">Email</label>
-                  <FormInput type="email" id="#email" placeholder="Email" />
-                </FormGroup>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col sm="12"><FormTextarea /></Col>
-              <Col sm="12"><Button block>Send Message</Button></Col>
-            </Row>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+          <Col>
+            <ul className="list-collection">
+              <li className="list-title light">Phone</li>
+              <li className="list-item light"><a >(616) 914-9356</a></li>
+              <li className="list-title light">Email</li>
+              <li className="list-item light"><a >hello@pixoulinc.com</a></li>
+            </ul>
+          </Col>
+          <Col>
+            <ContactForm />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
