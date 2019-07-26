c7816e093fb7fe7b2fc7b6608e10d433968172d4
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-26 14:10:26 -0500

    refractor: moves main routes into seperate component for easier management

--- src/app/app.js
+++ src/app/app.js
@@ -1,60 +1,31 @@
 import React, { useState } from "react"
-import { Route, Redirect, Switch, withRouter } from "react-router-dom"
-import {useTransition, animated, config} from 'react-spring'
 import "./app.scss"
 
 import Header from "layout/header/header"
 import Footer from "layout/footer/footer"
 import SideMenu from "layout/side-menu/side-menu"
-
-/* Page Components */
-import Home from "home/home"
-import About from "about/about"
-import Capabilities from "capabilities/capabilities"
-import Work from "work/work"
-import Contact from "contact/contact"
+import Routes from "./routes"
 
 function App(props) {
 
   const [open, toggleMenu] = useState(false);
 
-  const options = {
-    from: {
-      opacity: 0,
-      // transform: 'translate3d(0,100%,0)'
-    },
-    enter: {
-      opacity: 1,
-      // transform: 'translate3d(0,0%,0)'
-    },
-    leave: {
-      opacity: 0,
-      // transform: 'translate3d(0,-100%,0)'
-    }
-  }
-
-  const transitions = useTransition(props.location, location => location.pathname, options)
-
   const menuItems = [
     {
       label: 'Home',
-      route: '/home',
-      component: Home
+      route: '/home'
     },
     {
       label: 'Work',
-      route: '/work',
-      component: Work
+      route: '/work'
     },
     {
       label: 'Capabilities',
-      route: '/capabilities',
-      component: Capabilities
+      route: '/capabilities'
     },
     {
       label: 'About',
-      route: '/about',
-      component: About
+      route: '/about'
     }
   ]
 
@@ -62,25 +33,11 @@ function App(props) {
     <div>
       <SideMenu open={open} toggleMenu={toggleMenu} menuItems={menuItems}>
         <Header toggleMenu={() => toggleMenu(!open)} />
-           {transitions.map(({ item, props, key }) => (
-             <animated.div key={key} style={props}>
-                  <Switch location={item}>
-                      <Route path="/home" component={Home} />
-                      <Route path="/work" component={Work} />
-                      <Route path="/capabilities" component={Capabilities} />
-                      <Route path="/about" component={About} />
-                      <Route path="/contact" component={Contact} />
-
-                      <Route exact path='/' render={props => (
-                        <Redirect to={{ pathname: "/home", state: { from: props.location } }} />
-                      )}/>
-                  </Switch>
-              </animated.div>
-            ))}
+           <Routes />
         <Footer />
       </SideMenu>
     </div>
   );
 }
 
-export default withRouter(App)
+export default App
