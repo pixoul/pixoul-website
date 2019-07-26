dfbf2b01d3c6cba1ca120adc462ffd54ae9fae3f
Ryan Thompson <ryan@ryanthompson.io>
2019-07-25 20:21:21 -0500

    feature: implements sofs detail

---
+++ src/work/sofs/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import ClientSummary from "utils/client-summary/client-summary"
+
+import bg from "./images/bg.png"
+import logo from "./images/logo-color.jpg"
+
+export default function Sofs(){
+  return(
+    <ClientSummary
+      logo={logo}
+      action="/work/detail/sofs"
+      media={bg}
+      tagline="is taking field analytics to a whole new level."
+    />
+  )
+}
