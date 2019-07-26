fbf28e586575ab297f8836177fb543d205ceb266
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-25 20:21:21 -0500

    feature: implements sofs detail

--- src/app/app.js
+++ src/app/app.js
@@ -18,6 +18,7 @@ import Contact from "contact/contact"
 
 import IuzeitDetail from "work/iuzeit/detail"
 import GeorgiaPacificDetail from "work/georgia-pacific/detail"
+import SofsDetail from "work/sofs/detail"
 
 function App(props) {
 
@@ -64,6 +65,7 @@ function App(props) {
 
               <Route exact path="/work/detail/iuzeit" component={IuzeitDetail} />
               <Route exact path="/work/detail/georgia-pacific" component={GeorgiaPacificDetail} />
+              <Route exact path="/work/detail/sofs" component={SofsDetail} />
 
               <Route exact path='/' render={props => (
                 <Redirect to={{ pathname: "/home", state: { from: props.location } }} />
