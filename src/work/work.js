adb9133dcdd985e7b5bd1076997f3083e62e4be4
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-26 13:24:14 -0500

    cleanup: updates routes to use dynamic routing instead of all being in app.js

--- src/work/work.js
+++ src/work/work.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { withRouter } from "react-router-dom"
+import { withRouter, Route } from "react-router-dom"
 import Iuzeit from "work/iuzeit"
 import GeorgiaPacific from "work/georgia-pacific"
 import Sofs from "work/sofs"
@@ -7,7 +7,14 @@ import Fusemap from "work/fusemap"
 import Hcp from "work/hcp"
 import Ikarus from "work/ikarus"
 
-function Work({ match }){
+import IuzeitDetail from "work/iuzeit/detail"
+import GeorgiaPacificDetail from "work/georgia-pacific/detail"
+import SofsDetail from "work/sofs/detail"
+import FusemapDetail from "work/fusemap/detail"
+import HcpDetail from "work/hcp/detail"
+import IkarusDetail from "work/ikarus/detail"
+
+function Page(){
   return(
     <div>
       <Iuzeit />
@@ -16,6 +23,21 @@ function Work({ match }){
       <Fusemap />
       <Hcp />
       <Ikarus />
+    </div>
+  )
+}
+
+
+function Work({ match }){
+  return(
+    <div>
+      <Route exact path={`${match.path}/`} component={Page} />
+      <Route path={`${match.path}/detail/iuzeit`} component={IuzeitDetail} />
+      <Route path={`${match.path}/detail/georgia-pacific`} component={GeorgiaPacificDetail} />
+      <Route path={`${match.path}/detail/sofs`} component={SofsDetail} />
+      <Route path={`${match.path}/detail/fusemap`} component={FusemapDetail} />
+      <Route path={`${match.path}/detail/hcp`} component={HcpDetail} />
+      <Route path={`${match.path}/detail/ikarus`} component={IkarusDetail} />
     </div>
   )
 }
