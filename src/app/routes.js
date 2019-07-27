9c02042b93b2bd5a9c56978e4a1fe2f3fc5c0786
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-26 21:16:28 -0500

    cleanup: changes route animation to left to right and adds ref for page scroll after transition

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useRef } from "react"
 import { Route, Redirect, Switch, withRouter, __RouterContext } from "react-router-dom"
 import { useTransition, animated } from 'react-spring'
 
@@ -10,14 +10,16 @@ import Work from "work/work"
 import Contact from "contact/contact"
 
 function Routes(props){
+  const pageTop = useRef(null);
+
   const options = {
     initial: {
       opacity: 0,
-      transform: 'translate3d(100%, 0, 0)'
+      transform: 'translate3d(-100%, 0, 0)'
     },
     from: {
       opacity: 0,
-      transform: 'translate3d(100%, 0, 0)'
+      transform: 'translate3d(-100%, 0, 0)'
     },
     enter: {
       opacity: 1,
@@ -25,7 +27,10 @@ function Routes(props){
     },
     leave: {
       opacity: 0,
-      transform: 'translate3d(-100%, 0, 0)'
+      transform: 'translate3d(100%, 0, 0)'
+    },
+    onRest: () => {
+      pageTop.current.scrollIntoView({behavior: 'smooth'})
     }
   }
 
@@ -34,20 +39,21 @@ function Routes(props){
   const transitions = useTransition(location, location => location.pathname, options)
 
   return transitions.map(({ item, props : style, key }) => (
-    <animated.div key={key} style={{...style, position: "absolute", height: "100%", width: "100%"}}>
-         <Switch location={item}>
-             <Route path="/home" component={Home} />
-             <Route path="/work" component={Work} />
-             <Route path="/capabilities" component={Capabilities} />
-             <Route path="/about" component={About} />
-             <Route path="/contact" component={Contact} />
+      <animated.div key={key} style={{...style, position: "absolute", height: "100%", width: "100%"}} ref={pageTop}>
+           <Switch location={item}>
+               <Route path="/home" component={Home} />
+               <Route path="/work" component={Work} />
+               <Route path="/capabilities" component={Capabilities} />
+               <Route path="/about" component={About} />
+               <Route path="/contact" component={Contact} />
 
-             <Route exact path='/' render={props => (
-               <Redirect to={{ pathname: "/home", state: { from: props.location } }} />
-             )}/>
-         </Switch>
-     </animated.div>
-   ))
+               <Route exact path='/' render={props => (
+                 <Redirect to={{ pathname: "/home", state: { from: props.location } }} />
+               )}/>
+           </Switch>
+       </animated.div>
+   )
+  )
 }
 
 export default withRouter(Routes)
