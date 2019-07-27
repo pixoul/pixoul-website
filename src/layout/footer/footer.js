e144d74d8c655e30308a242c19421de763a3eacc
Ryan Thompson <ryan@ryanthompson.io>
2019-07-26 19:50:24 -0500

    refractor: updates footer to use flexbox instead of react-shards

--- src/layout/footer/footer.js
+++ src/layout/footer/footer.js
@@ -1,38 +1,41 @@
 import React from "react"
+import cn from "classnames"
 import { Link } from "react-router-dom";
-import { Container, Row, Col, Button } from "shards-react";
+import { Button } from "shards-react";
 
 import "./footer.scss"
 
-export default function Footer(){
-  return(
-    <footer className="footer">
-      <Container>
-        <Row>
-          <Col>
-            <ul className="list-collection">
-              <li className="list-title">Pixoul, Inc.</li>
-              <li className="list-item"><Link to={'/about'}>About</Link></li>
-              <li className="list-item"><Link to={'/work'}>Work</Link></li>
-              <li className="list-item"><Link to={'/capabilities'}>Capabilities</Link></li>
-            </ul>
+export default function Footer({ theme = 'dark' }){
 
-            <ul className="list-collection">
-              <li className="list-title">Capabilities</li>
-              <li className="list-item"><Link to={'/capabilities/strategy'}>Strategy</Link></li>
-              <li className="list-item"><Link to={'/capabilities/design'}>Design</Link></li>
-              <li className="list-item"><Link to={'/capabilities/technology'}>Technology</Link></li>
-            </ul>
-          </Col>
-          <Col>
-            <ul className="list-collection right">
-              <li className="list-subtitle">We average a 211% increase on ROI.</li>
-              <li className="list-item">See how we help ambitous brands use design thinking to dominate digital.</li>
-              <li className="list-item"><Button theme="primary" tag={Link} to={'/contact'}>Contact Us</Button></li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+  const classes = cn('footer', {
+    'light': theme === 'light',
+    'dark': theme === 'dark'
+  })
+
+  return(
+    <footer className={classes}>
+      <div className="footer-side left">
+        <ul className="list-collection">
+          <li className="list-title">Pixoul, Inc.</li>
+          <li className="list-item"><Link to={'/about'}>About</Link></li>
+          <li className="list-item"><Link to={'/work'}>Work</Link></li>
+          <li className="list-item"><Link to={'/capabilities'}>Capabilities</Link></li>
+        </ul>
+
+        <ul className="list-collection">
+          <li className="list-title">Capabilities</li>
+          <li className="list-item"><Link to={'/capabilities/strategy'}>Strategy</Link></li>
+          <li className="list-item"><Link to={'/capabilities/design'}>Design</Link></li>
+          <li className="list-item"><Link to={'/capabilities/technology'}>Technology</Link></li>
+        </ul>
+      </div>
+      <div className="footer-side right">
+        <ul className="list-collection">
+          <li className="list-subtitle">We average a 211% increase on ROI.</li>
+          <li className="list-item">See how we help ambitous brands use design thinking to dominate digital.</li>
+          <li className="list-item"><Button theme="primary" tag={Link} to={'/contact'}>Contact Us</Button></li>
+        </ul>
+      </div>
     </footer>
   )
 }
