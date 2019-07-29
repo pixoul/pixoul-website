e144f5d7f463dcc23fe433c2584692459ae41809
Ryan Thompson <ryan@ryanthompson.io>
2019-07-29 11:50:12 -0500

    fix: corrects issue where contact page displayed color weird and updates opening component to accept theme prop

--- src/utils/opening/opening.js
+++ src/utils/opening/opening.js
@@ -1,17 +1,27 @@
 import React from "react"
 import "./opening.scss"
+import cn from "classnames"
 
-export default function Opening(props){
+export default function Opening({
+  width = 800,
+  theme = 'dark',
+  title,
+  subtitle
+}){
 
-  const styles = {
-    width: props.width ? props.width : 800
-  }
+  const styles = {}
 
+  if(width) styles.width = width
+
+  const classes = cn('opening', {
+    'light': theme === 'light',
+    'dark': theme === 'dark'
+  })
 
   return(
-    <div className="opening" style={styles}>
-      <h3 className="opening-title">{props.title}</h3>
-      <p className="opening-subtitle">{props.subtitle}</p>
+    <div className={classes} style={styles}>
+      <h3 className="opening-title">{title}</h3>
+      <p className="opening-subtitle">{subtitle}</p>
     </div>
   )
 }
