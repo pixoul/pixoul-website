48484487f70db64cac6f427fef1407edc43012a9
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-30 18:29:23 -0500

    fix: updates client card to accept action and links actions in latest work

--- src/utils/client-card/client-card.js
+++ src/utils/client-card/client-card.js
@@ -1,13 +1,14 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import {Card, CardImg, CardBody} from "shards-react"
 import "./client-card.scss"
 
-export default function ClientCard(props){
+export default function ClientCard({ media, title, action}){
   return(
-    <Card className="client-card">
-      <CardImg top src={props.media} className="client-media" />
+    <Card className="client-card" tag={Link} to={action}>
+      <CardImg top src={media} className="client-media" />
       <CardBody>
-        <h4 className="client-title">{props.title}</h4>
+        <h4 className="client-title">{title}</h4>
       </CardBody>
     </Card>
   )
