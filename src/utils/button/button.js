b78e834bb630735463293e8e7feaeea8f4f2e766
Ryan Thompson <ryan@ryanthompson.io>
2019-07-30 15:31:22 -0500

    feature: implements basic button for menu footer to use instead of shards

--- src/utils/button/button.js
+++ src/utils/button/button.js
@@ -1,9 +1,15 @@
 import React from "react"
+import cn from "classnames"
+import "./button.scss"
 
-const Button = ({ children }) => {
+const Button = ({ children, tag, theme = 'primary' }) => {
+
+  const classes = cn('button', {
+    'button-primary': theme === 'primary'
+  })
 
   return(
-    <a className="button">
+    <a className={classes}>
       {children}
     </a>
   )
