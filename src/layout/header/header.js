c2d466c1cbe206a5f00c41c96f2c6c0e87c3b4de
Ryan Thompson <ryan@ryanthompson.io>
2019-07-30 19:38:12 -0500

    fix: implements hover icon on menu icon

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import cn from "classnames"
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from "shards-react";
@@ -9,12 +9,15 @@ import { toggleMenu } from "layout/navigation-menu/actions"
 
 import logo from "./images/logo.svg"
 import menu from "./images/menu.svg"
+import menuHover from "./images/menu-hover.svg"
 
 function Header({ fixed = false, open, toggleMenu }) {
 
   const classes = cn('header-container', {
     'fall-under': fixed === true
   })
+
+  const [hover, setHover] = useState(false)
 
   return (
     <div className={classes}>
@@ -25,7 +28,14 @@ function Header({ fixed = false, open, toggleMenu }) {
             <Button outline tag={Link} to="/contact">Contact Us</Button>
           </NavItem>
           <NavItem>
-            <NavLink className="menu-icon" onClick={() => toggleMenu(!open)} ><img src={menu} alt={menu} draggable="false" /></NavLink>
+            <NavLink
+              className="menu-icon"
+              onClick={() => toggleMenu(!open)}
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+            >
+              {hover ? <img src={menuHover} alt={menuHover} draggable="false" /> : <img src={menu} alt={menu} draggable="false" />}
+            </NavLink>
           </NavItem>
         </Nav>
       </Navbar>
