2507f064ea872350abfe6aad468cf5f2fa1ec423
Ryan Thompson <rjthompson@rjthompson.local>
2019-07-30 21:50:08 -0500

    cleanup: cleans capability component styling up and adds a few new props for equal and background

--- src/capabilities/partner-industries/partner-industries.js
+++ src/capabilities/partner-industries/partner-industries.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./partner-industries.scss"
 
 import { ParallelDisplay, ParallelContent, ParallelList, ParallelItem, ParallelImage, ParallelText } from "utils/parallel-display/parallel-display"
+import Capability from "utils/capability/capability"
 
 import example1 from "./images/example-1.jpg"
 import example2 from "./images/example-2.jpg"
@@ -11,41 +12,34 @@ import example5 from "./images/example-5.jpg"
 import example6 from "./images/example-6.jpg"
 import example7 from "./images/example-7.jpg"
 
+const partnerIndustries = {
+  direction: 'ltr',
+  background: 'white',
+  equal: true,
+  title: 'We’re contributing to every industry.',
+  subtitle: 'From venture-backed startups to the Fortune 500, we build solutions for our partners across a variety of industries.  Here’s just a few.',
+  details: {
+    'Business': 'B2B sales, internal tools, enterprise platforms, etc.',
+    'FinTech': 'Cryptocurrency, governance platforms, etc.',
+    'Government' : 'Local municipalities, economic boards, etc.',
+    'Healthcare' : 'Employee training, carrier messaging platforms, etc.'
+  },
+  media: [
+    { image: example1, width: 300 },
+    { image: example2, width: 400 },
+    { image: example3, width: 300 },
+    { image: example4, width: 400 },
+    { image: example5, width: 300 },
+    { image: example6, width: 200, height: 350 },
+    { image: example7, width: 200, height: 350 }
+  ]
+}
+
+
 export default function PartnerIndustries(){
   return(
     <div className="partner-industries">
-      <ParallelDisplay>
-        <ParallelContent multiple>
-          <ParallelImage media={example1} width={300} />
-          <ParallelImage media={example2} width={400} />
-          <ParallelImage media={example3} width={300} />
-          <ParallelImage media={example4} width={400} />
-          <ParallelImage media={example5} width={300} />
-          <ParallelImage media={example6} width={200} height={350} />
-          <ParallelImage media={example7} width={200} height={350} />
-        </ParallelContent>
-
-        <ParallelContent>
-          <ParallelText
-            width={550}
-            size={45}
-            color="#000000"
-            text="We’re contributing to every industry."
-          />
-          <ParallelText
-            width={529}
-            size={20}
-            text="From venture-backed startups to the Fortune 500, we build solutions for our partners across a variety of industries.  Here’s just a few."
-          />
-
-          <ParallelList>
-            <ParallelItem title="Business" tagline="B2B sales, internal tools, enterprise platforms, etc." />
-            <ParallelItem title="FinTech" tagline="Cryptocurrency, governance platforms, etc." />
-            <ParallelItem title="Government" tagline="Local municipalities, economic boards, etc." />
-            <ParallelItem title="Healthcare" tagline="Employee training, carrier messaging platforms, etc." />
-          </ParallelList>
-        </ParallelContent>
-      </ParallelDisplay>
+      <Capability {...partnerIndustries} />
     </div>
   )
 }
