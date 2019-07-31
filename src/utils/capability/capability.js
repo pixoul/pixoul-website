c4e662a57c3d0775ea6309d51a185cb4eddc6b43
Ryan Thompson <ryan@ryanthompson.io>
2019-07-30 21:50:08 -0500

    cleanup: cleans capability component styling up and adds a few new props for equal and background

--- src/utils/capability/capability.js
+++ src/utils/capability/capability.js
@@ -2,17 +2,25 @@ import React from "react"
 import "./capability.scss"
 import cn from "classnames"
 
-const Capability = ({ title, subtitle, details = {}, media, direction = 'rtl' }) => {
+const Capability = ({
+  title,
+  subtitle,
+  details = {},
+  media,
+  direction = 'rtl',
+  background = direction === 'rtl' ? '#ffffff' : '#f9fafc',
+  equal = false
+}) => {
 
   const classes = cn('capability', {
     'rtl': direction === 'rtl',
-    'ltr': direction === 'ltr'
+    'ltr': direction === 'ltr',
+    'equal': equal
   })
 
   return(
-    <div className={classes}>
-        <div className="capability-column">
-          <div className="capability-content">
+    <div className={classes} style={{ backgroundColor: background }}>
+        <div className="capability-column capability-content">
             <div className="capability-title">{title}</div>
             <div className="capability-subtitle">{subtitle}</div>
 
@@ -24,13 +32,12 @@ const Capability = ({ title, subtitle, details = {}, media, direction = 'rtl' })
                 </div>
               ))}
             </div>
-          </div>
         </div>
         <div className="capability-column">
           <div className="capability-media">
             { Array.isArray(media) ? media.map((item) => (
-              <img src={item.image} alt={item.image} draggable="false" className="capability-image" {...item}/>
-            )) : <img src={media.image} alt={media.image} draggable="false" className="capability-image" {...media} />}
+              <img src={item.image} alt={item.image} draggable="false" className="capability-image" width={item.width} height={item.height} />
+            )) : <img src={media.image} alt={media.image} draggable="false" className="capability-image" width={media.width} height={media.height} />}
           </div>
         </div>
     </div>
