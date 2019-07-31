07d1a15ac3abe74027e2a3188c5824abb14b2c5f
Ryan Thompson <ryan@ryanthompson.io>
2019-07-31 11:08:45 -0500

    fix: corrects redux pathing issues

--- src/contact/contact-form/contact-form.js
+++ src/contact/contact-form/contact-form.js
@@ -1,31 +1,65 @@
-import React from "react"
+import React, { useState } from "react"
 import "./contact-form.scss"
 import { Row, Col, Form, FormInput, FormGroup, FormTextarea, Button } from "shards-react";
+import * as emailjs from 'emailjs-com';
 
 export default function ContactForm(){
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [sent, setSent] = useState(false)
+
+  const send = e => {
+   e.preventDefault();
+   const serviceId = "default_service"
+   const templateId = "template_3l1ZvrRp"
+
+   const data = {
+      "email": email,
+      "name": name,
+      "message": message
+    }
+
+   emailjs.send(serviceId, templateId, data, 'user_y5b1msGPAYKIW4szoHygG')
+    .then((response) => {
+        setSent(true)
+    }, (err) => {
+        setSent(false)
+        alert('Failed, please try again later');
+    });
+  }
+
   return(
     <div className="contact-form">
-      <Form className="form">
-        <Row>
-          <Col>
-            <FormGroup>
-              <label htmlFor="#name">Name</label>
-              <FormInput id="#name" placeholder="Name" />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <label htmlFor="#email">Email</label>
-              <FormInput type="email" id="#email" placeholder="Email" />
-            </FormGroup>
-          </Col>
-        </Row>
+      { sent ?
+        <div className="contact-message">
+          <p className="success">Thank you! Someone will be in touch with you soon.</p>
+          <Button className="form-button" onClick={() => setSent(false)}>Send Another Message</Button>
+        </div>
+        :
+        <Form className="form">
+          <Row>
+            <Col>
+              <FormGroup>
+                <label htmlFor="#name">Name</label>
+                <FormInput id="#name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <label htmlFor="#email">Email</label>
+                <FormInput type="email" id="#email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+              </FormGroup>
+            </Col>
+          </Row>
 
-        <Row>
-          <Col sm="12"><FormTextarea className="form-message" /></Col>
-          <Col sm="12"><Button block className="form-button">Send Message</Button></Col>
-        </Row>
-      </Form>
+          <Row>
+            <Col sm="12"><FormTextarea className="form-message" value={message} onChange={(e) => setMessage(e.target.value)} /></Col>
+            <Col sm="12"><Button block className="form-button" onClick={send}>Send Message</Button></Col>
+          </Row>
+        </Form>
+      }
     </div>
   )
 }
