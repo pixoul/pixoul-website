0f2c542b1620bd6bd64e74de0d5ed1c97429da20
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-01 17:12:40 -0500

    refactor: updates the contact page to be inline with new design and removes route transitions

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -2,40 +2,37 @@ import React from "react";
 import { Container, Row, Col } from "shards-react";
 import "./contact.scss"
 
-import Header from "layout/header/header"
-import Footer from "layout/footer/footer"
 import Opening from "utils/opening/opening"
+import SocialIcons from "utils/social-icons/social-icons"
 import ContactForm from "contact/contact-form/contact-form"
 
 export default function Contact() {
   return(
-    <div>
-      <Header />
-        <div className="contact">
-            <Container>
-              <Opening
-                title="Contact Us"
-                subtitle="See how we help ambitious brands use design thinking to dominate digital."
-                theme="light"
-              />
+    <div className="contact">
+        <Container>
+          <Opening
+            title="Contact Us"
+            subtitle="See how we help ambitious brands use design thinking to dominate digital."
+          />
 
-              <Row>
-                <Col>
-                  <ul className="list-collection">
-                    <li className="list-title light">Phone</li>
-                    <li className="list-item light"><a href="tel:6169149356">(616) 914-9356</a></li>
-                    <li className="list-title light">Email</li>
-                    <li className="list-item light"><a href="mailto:hello@pixoulinc.com">hello@pixoulinc.com</a></li>
-                  </ul>
-                </Col>
-                <Col>
-                  <ContactForm />
-                </Col>
-              </Row>
-            </Container>
-        </div>
+          <Row>
+            <Col>
+              <ul className="contact-info">
+                <li className="contact-title">Phone</li>
+                <li className="contact-item"><a href="tel:6169149356">(616) 914-9356</a></li>
+                <li className="contact-title">Email</li>
+                <li className="contact-item"><a href="mailto:hello@pixoulinc.com">hello@pixoulinc.com</a></li>
+              </ul>
 
-      <Footer theme="light" />
+              <div className="social-media">
+                <SocialIcons theme="dark" />
+              </div>
+            </Col>
+            <Col>
+              <ContactForm />
+            </Col>
+          </Row>
+        </Container>
     </div>
   )
 }
