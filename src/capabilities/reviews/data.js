5099f45016f5da64edb88dcad983c3730b07426c
Ryan Thompson <ryan@ryanthompson.io>
2019-08-02 15:19:40 -0500

    feature: adds reviews to a carousel and updates review card styling

---
+++ src/capabilities/reviews/data.js
@@ -0,0 +1,55 @@
+
+import reviewer1 from "./images/reviewer-1.jpg"
+import reviewer2 from "./images/reviewer-2.jpg"
+import reviewer3 from "./images/reviewer-3.jpg"
+import reviewer4 from "./images/reviewer-4.svg"
+import reviewer5 from "./images/reviewer-5.svg"
+import reviewer6 from "./images/reviewer-6.svg"
+
+const list = []
+
+
+list.push({
+  avatar: reviewer1,
+  reviewer: 'Kendall Sampson',
+  designation: 'Product Manager',
+  review: "Pixoul is absolutely amazing!  They guided us through our website redesign and held our hand every step of the way. I couldn't have asked for a better team."
+})
+
+list.push({
+  avatar: reviewer2,
+  reviewer: 'Jake Richards',
+  designation: 'CTO',
+  review: "Used Pixoul after two failed attempts at other dev agencies.  Since then, we've seen engagement go through the roof and customers are loving our new platform."
+})
+
+list.push({
+  avatar: reviewer3,
+  reviewer: 'Quincy Roberts',
+  designation: 'CTO',
+  review: "Pixoul spent the day at our offices explaining design thinking fundamentals. We constantly refer back to those sessions for every decision we make!!"
+})
+
+list.push({
+  avatar: reviewer4,
+  reviewer: 'Danni Parsons',
+  designation: 'CMO',
+  review: "The team at Pixoul is incredible. We came to them with a huge list of requests and they created a beautiful, functional enterprise system for us. It was a big undertaking and their whole team pulled it off."
+})
+
+list.push({
+  avatar: reviewer5,
+  reviewer: 'Derrick Bradley',
+  designation: 'VP of Operations',
+  review: "Thank you to the entire Pixoul team for your all-hands-on-deck mentality. Our customers are over the moon about our new site and we’re reaping the rewards."
+})
+
+list.push({
+  avatar: reviewer6,
+  reviewer: 'Josh Porrough',
+  designation: 'Digital Director',
+  review: "Pixoul is more than great design. Their team helped us pull together a digital roadmap, determine priorities, and organize our efforts from start to finish."
+})
+
+
+export default list
