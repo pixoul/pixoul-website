5fb35468e56cf0b3687b2b9faf501b957699477f
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-07 17:15:57 -0500

    refractor: enhances button utility

--- src/utils/button/button.js
+++ src/utils/button/button.js
@@ -1,27 +1,36 @@
 import React from "react"
+import PropTypes from "prop-types"
 import cn from "classnames"
 import "./button.scss"
 
-const Button = ({ children, tag, theme = 'primary', ...rest }) => {
+const Button = ({
+  className,
+  children,
+  tag: Tag,
+  theme = 'primary',
+  outline = false,
+  block = false,
+  ...attrs
+}) => {
 
-  const classes = cn('button', {
-    'button-primary': theme === 'primary'
-  })
-
-  if(tag){
-    const Tag = tag;
-    return(
-      <Tag {...rest} className={classes}>
-        {children}
-      </Tag>
-    )
-  }
+  const classes = cn(
+    className,
+    'button',
+    {
+      'button-primary': theme === 'primary',
+      'button-outline': outline,
+      'button-block': block
+    })
 
   return(
-    <button className={classes} {...rest}>
+    <Tag {...attrs} className={classes}>
       {children}
-    </button>
+    </Tag>
   )
+}
+
+Button.defaultProps = {
+  tag: "button"
 }
 
 export default Button
