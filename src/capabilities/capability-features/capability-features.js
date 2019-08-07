b4e82786cd5eabf19573813ebd629a22ca26e145
Ryan Thompson <ryan@ryanthompson.io>
2019-08-07 17:47:52 -0500

    fix: adds grid back to capability features

--- src/capabilities/capability-features/capability-features.js
+++ src/capabilities/capability-features/capability-features.js
@@ -11,71 +11,41 @@ import brush from "./images/brush.svg"
 import code from "./images/code.svg"
 
 const CapabilityFeatures = () => {
-  // return(
-  //   <Container className="capability-features">
-  //     <Row>
-  //       <Col sm="12" md="4">
-  //         <Feature width={343} >
-  //           <FeatureHeader>
-  //             <FeatureIcon svg={power} />
-  //             <FeatureTitle text="Strategy" />
-  //           </FeatureHeader>
-  //           <FeatureTagline text="Reimagine and redefine your organizational approach by developing new ways of attaining mission-critical KPIs." />
-  //           <FeatureAction link="/capabilities/strategy" text="Learn More" />
-  //         </Feature>
-  //       </Col>
-  //       <Col sm="12" md="4">
-  //         <Feature width={343}>
-  //           <FeatureHeader>
-  //             <FeatureIcon svg={brush} />
-  //             <FeatureTitle text="Design Thinking" />
-  //           </FeatureHeader>
-  //           <FeatureTagline text="Use Design Thinking to create products, services, systems, and experiences that address core needs of your target users." />
-  //           <FeatureAction link="/capabilities/design" text="Learn More" />
-  //         </Feature>
-  //       </Col>
-  //       <Col sm="12" md="4">
-  //         <Feature width={343}>
-  //           <FeatureHeader>
-  //             <FeatureIcon svg={code} />
-  //             <FeatureTitle text="Technology" />
-  //           </FeatureHeader>
-  //           <FeatureTagline text="Bring your digital solution to life with cutting-edge infrastructure that can scale across devices and platforms." />
-  //           <FeatureAction link="/capabilities/technology" text="Learn More" />
-  //         </Feature>
-  //       </Col>
-  //     </Row>
-  //   </Container>
-  // )
   return(
-    <div className="capability-features">
-        <Feature width={343} >
-          <FeatureHeader>
-            <FeatureIcon svg={power} />
-            <FeatureTitle text="Strategy" />
-          </FeatureHeader>
-          <FeatureTagline text="Reimagine and redefine your organizational approach by developing new ways of attaining mission-critical KPIs." />
-          <FeatureAction link="/capabilities/strategy" text="Learn More" />
-        </Feature>
-
-        <Feature width={343}>
-          <FeatureHeader>
-            <FeatureIcon svg={brush} />
-            <FeatureTitle text="Design Thinking" />
-          </FeatureHeader>
-          <FeatureTagline text="Use Design Thinking to create products, services, systems, and experiences that address core needs of your target users." />
-          <FeatureAction link="/capabilities/design" text="Learn More" />
-        </Feature>
-
-        <Feature width={343}>
-          <FeatureHeader>
-            <FeatureIcon svg={code} />
-            <FeatureTitle text="Technology" />
-          </FeatureHeader>
-          <FeatureTagline text="Bring your digital solution to life with cutting-edge infrastructure that can scale across devices and platforms." />
-          <FeatureAction link="/capabilities/technology" text="Learn More" />
-        </Feature>
-    </div>
+    <Container className="capability-features">
+      <Row>
+        <Col sm={12} md={4}>
+          <Feature>
+            <FeatureHeader>
+              <FeatureIcon svg={power} />
+              <FeatureTitle text="Strategy" />
+            </FeatureHeader>
+            <FeatureTagline text="Reimagine and redefine your organizational approach by developing new ways of attaining mission-critical KPIs." />
+            <FeatureAction link="/capabilities/strategy" text="Learn More" />
+          </Feature>
+        </Col>
+        <Col sm={12} md={4}>
+          <Feature>
+            <FeatureHeader>
+              <FeatureIcon svg={brush} />
+              <FeatureTitle text="Design Thinking" />
+            </FeatureHeader>
+            <FeatureTagline text="Use Design Thinking to create products, services, systems, and experiences that address core needs of your target users." />
+            <FeatureAction link="/capabilities/design" text="Learn More" />
+          </Feature>
+        </Col>
+        <Col sm={12} md={4}>
+          <Feature>
+            <FeatureHeader>
+              <FeatureIcon svg={code} />
+              <FeatureTitle text="Technology" />
+            </FeatureHeader>
+            <FeatureTagline text="Bring your digital solution to life with cutting-edge infrastructure that can scale across devices and platforms." />
+            <FeatureAction link="/capabilities/technology" text="Learn More" />
+          </Feature>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
