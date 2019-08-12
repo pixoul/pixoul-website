523836ce463054b0c23c860b82bfdbcb907cac34
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-12 14:39:22 -0500

    refractor: updated header to look like new design

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -3,59 +3,49 @@ import { Link } from "react-router-dom";
 import cn from "classnames"
 import "./header.scss"
 
-import HamburgerMenu from "utils/burger-menu/burger-menu"
 import Button from "utils/button/button"
-
-import { connect } from 'react-redux'
-import { toggleMenu } from "layout/navigation/actions"
-
 import logo from "./images/logo.svg"
 
-function Header({ fixed = false, open, toggleMenu }) {
+function Header({ fixed = false }) {
 
   const classes = cn('header-container', {
     'fall-under': fixed === true
   })
 
-  const [hover, setHover] = useState(false)
-
   return(
     <div className={classes}>
-      <div className="header-brand">
-        <Link to="/home">
-          <img src={logo} alt={logo} draggable="false" />
-        </Link>
-      </div>
-      <ul className="header-items">
-        <li className="header-item">
-          <Button tag={Link} to="/contact" outline>Contact Us</Button>
+      <ul className="header-links">
+        <li className="header-link">
+          <Link to="/home">
+            <img src={logo} alt={logo} draggable="false" />
+          </Link>
         </li>
-        <li className="header-item header-link">
-          <HamburgerMenu open={open} onClick={() => toggleMenu(!open)} />
+        <li className="header-link">
+          <Link to="/about">
+            About
+          </Link>
+        </li>
+        <li className="header-link">
+          <Link to="/talent">
+            Talent
+          </Link>
+        </li>
+        <li className="header-link">
+          <Link to="/process">
+            Process
+          </Link>
+        </li>
+        <li className="header-link">
+          <Link to="/faq">
+            FAQ
+          </Link>
         </li>
       </ul>
+      <div className="header-action">
+        <Button tag={Link} to="/contact" outline theme="light">Log In</Button>
+      </div>
     </div>
   )
 }
 
-
-const mapStateToProps = (state, ownProps) => {
-  return {
-    open: state.navigation.open
-  }
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    toggleMenu: (open) => {
-      dispatch(toggleMenu(open))
-    }
-  }
-}
-
-const HeaderToggle = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Header)
-
-export default HeaderToggle
+export default Header
