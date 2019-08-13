5a7e14cf6df25af4e8f7e4e6b6c0270100ad6e61
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-13 13:16:13 -0500

    feature: creates starter reviews section on home until content available

---
+++ src/home/reviews.js
@@ -0,0 +1,37 @@
+import React from "react"
+/* Utils */
+import { Carousel, Slide } from "utils/carousel"
+import Review from "utils/review-card/review-card"
+
+/* Images */
+import reviewer1 from "./images/reviewers/reviewer-1.png"
+
+const Reviews = () => {
+
+  return(
+    <div className="reviews gutters">
+      <h3 className="reviews-title">WHAT OUR CLIENTS SAY</h3>
+
+      <Carousel maxSlides={1} offset={76} gutter={20} autoplay>
+        <Slide>
+          <Review
+            review="The process is very low pressure, and highly customizable to our budget and goals.  Highly recommend!"
+            avatar={reviewer1}
+            reviewer="JOSH EVANS"
+            designation="Chief Technology Officer"
+          />
+        </Slide>
+        <Slide>
+          <Review
+            review="The process is very low pressure, and highly customizable to our budget and goals.  Highly recommend!"
+            avatar={reviewer1}
+            reviewer="JOSH EVANS"
+            designation="Chief Technology Officer"
+          />
+        </Slide>
+      </Carousel>
+    </div>
+  )
+}
+
+export default Reviews
