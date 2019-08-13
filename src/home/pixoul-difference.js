f2b1a993a337642f46cc638447d77f43fdf825ea
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-13 09:54:03 -0500

    refractor: implements new feature component with position property and finishes pixoul difference component

--- src/home/pixoul-difference.js
+++ src/home/pixoul-difference.js
@@ -21,27 +21,53 @@ const PixoulDifference = () => {
 
       <Container>
         <Row>
-          <Col sm={12} md={6} lg={4}>
+          <Col sm={12} md={6}>
             <Feature
+              position="left"
               title="TOP 1% GUARANTEED"
               media={thumbsup}
               description="Our 7-step hiring process ensures you work with only the best, most experienced freelancers who are actively building their own businesses."
             />
           </Col>
-          <Col sm={12} md={6} lg={4}>
-
+          <Col sm={12} md={6}>
+            <Feature
+              position="left"
+              title="RISK FREE TRIAL"
+              media={money}
+              description="At Pixoul, we’re confident that we’ll find you the perfect match.  In fact, we’re so confident that we provide a risk-free trial for you to ensure smooth onboarding."
+            />
           </Col>
-          <Col sm={12} md={6} lg={4}>
-
+          <Col sm={12} md={6}>
+            <Feature
+              position="left"
+              title="AI-POWERED, WHITE GLOVE TREATMENT"
+              media={heart}
+              description="Our powerful AI matching helps narrow down your talent pool by matching on skills, industry experience, availability, location, and more.  Then, Pixoul hand-selects and presents your team for approval."
+            />
           </Col>
-          <Col sm={12} md={6} lg={4}>
-
+          <Col sm={12} md={6}>
+            <Feature
+              position="left"
+              title="FULL TEAM SUPPORT"
+              media={lightning}
+              description="Our network spans the digital project lifecycle—from developers to full scrum teams that include UI/UX designers, business analysts, QA/testers, devops, db admins, and project managers."
+            />
           </Col>
-          <Col sm={12} md={6} lg={4}>
-
+          <Col sm={12} md={6}>
+            <Feature
+              position="left"
+              title="ON DEMAND EXPERTISE"
+              media={clock}
+              description="Pixoul matches you with top tech talent within your ideal time table.  By using senior-level professionals, your project will hit the ground running fast."
+            />
           </Col>
-          <Col sm={12} md={6} lg={4}>
-
+          <Col sm={12} md={6}>
+            <Feature
+              position="left"
+              title="DEDICATED ACCOUNT MANAGER"
+              media={user}
+              description="You’ll be matched with a dedicated Account Manager who is always available to ensure a lasting relationship with your freelancer, and guide you every step of the way."
+            />
           </Col>
         </Row>
       </Container>
