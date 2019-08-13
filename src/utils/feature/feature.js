8214976865dbe75678dceff7c47fb8e95ed09041
Ryan Thompson <ryan@ryanthompson.io>
2019-08-13 09:54:03 -0500

    refractor: implements new feature component with position property and finishes pixoul difference component

--- src/utils/feature/feature.js
+++ src/utils/feature/feature.js
@@ -1,21 +1,32 @@
 import React from "react"
 import "./feature.scss"
+/* Third-Party */
+import cn from "classnames"
 
 const Feature = ({
   title,
   media,
-  description
-}) => (
-  <div className="feature">
-    <div className="feature-media">
-      <img src={media} alt={media} />
-    </div>
-    <div className="feature-content">
-      <h4 className="feature-title">{title}</h4>
-      <p className="feature-description">{description}</p>
-    </div>
-  </div>
-)
+  description,
+  position = 'top-left'
+}) => {
 
+  const classes = cn('feature', {
+    'top-left': position === 'top-left',
+    'top-center': position === 'top-center',
+    'left': position === 'left'
+  })
+
+  return (
+    <div className={classes}>
+      <div className="feature-media">
+        <img src={media} alt={media} />
+      </div>
+      <div className="feature-content">
+        <h4 className="feature-title">{title}</h4>
+        <p className="feature-description">{description}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Feature
