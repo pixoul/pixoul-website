2543116d8fe07958c6fcda2ea84a462486be80e1
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-14 14:20:16 -0500

    feature: creates roadmap promo component on about page

---
+++ src/about/roadmap-promo.js
@@ -0,0 +1,24 @@
+import React from "react"
+/* Third-Party */
+import { Link } from "react-router-dom"
+/* Utils */
+import Button from "utils/button/button"
+/* Images */
+import roadmapPromo from "./images/roadmap-promo.png"
+
+const style = {
+  backgroundImage: 'url('+roadmapPromo+')'
+}
+
+const RoadmapPromo = () => (
+  <div className="roadmap-promo" style={style}>
+    <span className="roadmap-title">Let’s Improve Your Roadmap.</span>
+    <span className="roadmap-tagline">We average a 219% increase in ROI.  Find out how.</span>
+    <div className="roadmap-action">
+      <Button tag={Link} to="/contact">Get in Touch</Button>
+    </div>
+  </div>
+)
+
+
+export default RoadmapPromo
