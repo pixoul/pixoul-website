012c9d66bd20676531ee148ff90f532b6c55e747
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-14 17:03:39 -0500

    refractor: moves about intro to seperate component

--- src/about/about.js
+++ src/about/about.js
@@ -3,6 +3,7 @@ import "./about.scss"
 /* Utils */
 import Button from "utils/button/button"
 /* Presentational Components */
+import Intro from "./intro"
 import LoveMore from "./love-more"
 import RoadmapPromo from "./roadmap-promo"
 import Comparison from "./comparison"
@@ -16,22 +17,7 @@ export default function About() {
 
   return(
     <div className="about">
-      <div className="intro-section gutters">
-        <div className="intro-content">
-          <h4 className="intro-tagline">HIGH PERFORMING TALENT, ON DEMAND.</h4>
-          <h1 className="intro-title">Welcome to your elite talent network.</h1>
-          <p className="intro-text">We hire world-class designers, developers and project managers.  Pixoul talent contributes to the most innovative and profitable companies across the globe. </p>
-
-          <div className="button-group">
-            <Button theme="secondary">Hire the Perfect Match</Button>
-            <Button theme="secondary" outline>Join the Team</Button>
-          </div>
-        </div>
-        <div className="intro-media">
-          <img src={iphoneFront} alt={iphoneFront} className="intro-image front" />
-          <img src={iphoneBack} alt={iphoneBack} className="intro-image back" />
-        </div>
-      </div>
+      <Intro />
 
       <div className="info-grid">
         <div className="info-item ltr">
