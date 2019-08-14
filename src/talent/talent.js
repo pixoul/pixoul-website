95a8ec307fb51d14534dc6664dc7836bf416a91e
Ryan Thompson <ryan@ryanthompson.io>
2019-08-14 18:23:24 -0500

    feature: creates featured member component for talent page

--- src/talent/talent.js
+++ src/talent/talent.js
@@ -3,13 +3,14 @@ import "./talent.scss"
 
 /* Presentational Components */
 import Intro from "./intro"
-
+import FeaturedMember from "./featured-member"
 
 const Talent = () => {
 
   return(
     <div className="talent">
       <Intro />
+      <FeaturedMember />
     </div>
   )
 }
