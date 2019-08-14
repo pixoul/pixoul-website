637ca955557c72ad1e95be14204c964d0d040701
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-14 18:00:59 -0500

    feature: creates the intro section for talent page

--- src/talent/talent.js
+++ src/talent/talent.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./talent.scss"
 
+/* Presentational Components */
+import Intro from "./intro"
+
+
 const Talent = () => {
 
   return(
     <div className="talent">
-
+      <Intro />
     </div>
   )
 }
