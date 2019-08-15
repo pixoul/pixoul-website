c38138cfec34519039c000869a400fc380749794
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-15 01:58:10 -0500

    feature: creates client work component for talent page and fully implements

--- src/talent/client-work.js
+++ src/talent/client-work.js
@@ -1,10 +1,6 @@
 import React, { useState } from "react"
-/* Third-Party */
-import { Link } from "react-router-dom"
-import cn from "classnames"
 /* Utils */
-import Button from "utils/button/button"
-
+import Project from "utils/project/project"
 /* Images */
 import ikarus from "./images/ikarus.svg"
 import iuzeit from "./images/iuzeit.svg"
@@ -13,48 +9,69 @@ import georgiaPacific from "./images/georgia-pacific.svg"
 import fusemap from "./images/fusemap.svg"
 import hcp from "./images/hcp.svg"
 
-
-const ProjectMedia = ({
-  media,
-  name,
-  onClick
-}) => {
-
-  const [hover, setHover] = useState(false)
-
-  const classes = cn('project-overlay', {
-    'hover' : hover
-  })
+const ClientWork = () => {
 
   return (
-    <div className="project-media" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-      <img src={media} alt={media} className="project-image"/>
-
-      <div className={classes}>
-        <span className="project-name">{name}</span>
-        <Button onClick={onClick}>View Project</Button>
+    <div className="client-work gutters">
+      <div className="work-row">
+        <Project
+          media={ikarus}
+          name="Ikarūs"
+          tagline="Redefining the Business Marketplace"
+          description="Armed with their vision for a easier and more open business marketplace, we transformed how owners interact with each other, investors, and the public."
+          services={['UX/UI Design', 'Development', 'QA Testing']}
+          tools={['Balsamiq', 'Sketch', 'InVision', 'Atom']}
+        />
+        <Project
+          media={iuzeit}
+          name="iuzeit"
+          tagline="Shaping the ECommerce Journey"
+          description="We refined the challenges of a traditional e-commerce journey to create a markeplace that helps prioritize what products matter most—all in one sleek, modern interface."
+          services={['UX/UI Design', 'Brand Strategy']}
+          tools={['Adobe XD', 'Sketch', 'InVision', 'Zeplin']}
+        />
       </div>
+      <div className="work-row">
+        <Project
+          media={stallion}
+          name="Stallion Oilfield Services"
+          tagline="Taking Field Analytics To A New Level"
+          description="We took data vizualization to the next level by building a robust interface that simplified complex data streams and allowed snapshot statistics of vital KPIs."
+          services={['UX/UI Design', 'QA Testing', 'Development']}
+          tools={['Pen + Paper', 'Sketch', 'InVision', 'Trello']}
+        />
+        <Project
+          media={georgiaPacific}
+          name="Georgia-Pacific"
+          tagline="Transforming how Logistics Gets Done"
+          description="We tailor-fit a new check-in/check-out system for outdoor professionalsthat allowed Georgia-Pacific to quickly give their logisitics team an efficiency boost."
+          services={['Design Partnership', 'UX/UI Design']}
+          tools={['Balsamiq', 'Figma', 'Sketch', 'InVision']}
+        />
+        <div className="work-column">
+          <Project
+            media={fusemap}
+            name="Fusemap"
+            tagline="Thinking Smarter About How We Power The World"
+            description="Using their cloud-based tool, we captured and vizualized the complex operational tasks of automated and AI-powered building management."
+            services={['Project Management', 'UX/UI Design']}
+            tools={['Trello', 'Balsamiq', 'Sketch', 'InVision']}
+          />
+          <Project
+            media={hcp}
+            name="Healthcare Compliance Pros"
+            tagline="Setting the Standard in Healthcare-Literally"
+            description="Designing for a multi-billion user industry, we evolved the HCP’S online suite into a feature-rich tool that maintainS strict legal compliance, engages users, and fosters long-term retention."
+            services={['Project Management', 'UX/UI Design']}
+            tools={['Trello', 'Adobe XD', 'Sketch', 'InVision']}
+          />
+        </div>
+      </div>
+
+
     </div>
   )
 }
-
-
-const ClientWork = () => (
-  <div className="client-work gutters">
-    <div className="work-row">
-      <ProjectMedia media={ikarus} name="ikarus" onClick={() => console.log('clicked')} />
-      <ProjectMedia media={iuzeit} name="iuzeit" onClick={() => console.log('clicked')} />
-    </div>
-    <div className="work-row">
-      <ProjectMedia media={stallion} name="stallion" onClick={() => console.log('clicked')} />
-      <ProjectMedia media={georgiaPacific} name="Georgia Pacific" onClick={() => console.log('clicked')} />
-      <div className="work-column">
-        <ProjectMedia media={fusemap} name="Fusemap" onClick={() => console.log('clicked')} />
-        <ProjectMedia media={hcp} name="HCP" onClick={() => console.log('clicked')} />
-      </div>
-    </div>
-  </div>
-)
 
 
 export default ClientWork
