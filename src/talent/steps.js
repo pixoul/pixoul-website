5e845a4140ed30d3777361432ddddb5290af8ab5
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-14 20:04:39 -0500

    chore: adds data for talent timeline as steps file in talent folder

---
+++ src/talent/steps.js
@@ -0,0 +1,52 @@
+const list = []
+
+list.push({
+  header: 'Step 1 - 100%',
+  title: 'All Applicants',
+  description: 'Our applicants apply from around the world, seeking a network of business-minded IT professionals.',
+  color: 'clear'
+})
+
+list.push({
+  header: 'Step 2 - Top 40%',
+  title: 'Application Review',
+  description: 'Pixoul domain experts do a comprehensive review of every application, resume, and portfolio, searching for exceptional, well-rounded individuals.',
+  color: 'clear'
+})
+
+list.push({
+  header: 'Step 3 - Top 10%',
+  title: 'Screening Interview',
+  description: 'Beginning with a video conference, candidates must command a mastery of English reading, writing, and speaking, as well as show qualities of passion, determination, and a desire to grow their freelance business.',
+  color: 'clear'
+})
+
+list.push({
+  header: 'Step 4 - Top 3%',
+  title: 'Coding or Design Challenge',
+  description: 'Candidates at this stage are asked to complete design, research, or coding challneges.  Only those with expert-level comprehension will move move forward.',
+  color: 'white'
+})
+
+list.push({
+  header: 'Step 5 - Top 2%',
+  title: 'Skill Scoring',
+  description: 'We test for every major skill category, so that we know exactly how comfortable each candidate is in a given area of expertise.  This empowers AI and informs our white-glove matchmaking process.',
+  color: 'white'
+})
+
+list.push({
+  header: 'Step 6 - Top 1%',
+  title: 'Executive C-Suite Review',
+  description: 'Before we finalize anything, candidates go through one more screening interview with our C-Suite executives to ensure they meet our highest standards.',
+  color: 'blue'
+})
+
+list.push({
+  header: 'Step 7 - Top 1%',
+  title: 'One-On-One Assessment',
+  description: 'After being accepted into our network, our Team Members recieve weekly support from their business-strategy advisors and are expected to routinely learn and grow as technology advances.',
+  color: 'blue'
+})
+
+export default list
