84dc19b640f74e231a6b2176a9afc8b4c9887649
Ryan Thompson <ryan@ryanthompson.io>
2019-08-16 17:43:43 -0500

    chore: adds faq to routes

---
+++ src/faq/data.js
@@ -0,0 +1,74 @@
+const list = []
+
+list.push({
+  question: "WHO IS PIXOUL?",
+  answer: "We’re an on-demand, global network of IT professionals.  We source our senior-level designers, developers, and project managers to corporations across the globe to augment their staff or build entirely new solutions."
+})
+
+list.push({
+  question: "WHAT DOES PIXOUL OFFER?",
+  answer: "With our matchmaking services, we use a combination of AI and white-glove manual selection to match dedicated IT and digital team members with your exact project needs.  Think of our talent as a remote extension of your own team while Pixoul handles the HR, payroll, project management, and support."
+})
+
+list.push({
+  question: "ARE THERE ANY UPFRONT FEES OR RECRUITING COSTS?",
+  answer: "We’re not recruiters so there are no recruiter fees.  There is an up-front onboarding deposit once you’re ready to get started with your team member, which will be deducted from your first invoice."
+})
+
+list.push({
+  question: "DO ALL PIXOUL TEAM MEMBERS SPEAK FLUENT ENGLISH?",
+  answer: "Yes—of course.  We understand communication is a number one goal for our clients.  Every Pixoul team member must pass rigorous screening, including verbal and written tests.  As a result, nearly 90% of our talent is US based."
+})
+
+list.push({
+  question: "WHAT MAKES PIXOUL UNIQUE?",
+  answer: "At the end of the day, our talent that sets us apart.  Our team members are unparalleled communicators, able to collaborate with technical and non-technical teams alike.  Because they're experts in their field, onboarding time is minimal, and they deliver results nearly 3x faster than junior or overseas alternative."
+})
+
+list.push({
+  question: "WHAT MAKES PIXOUL DIFFERENT FROM OTHER MARKEPLACES OR AGENCIES?",
+  answer: "We take pride in our matchmaking approach, which utilizes both AI and a manual, hand-selected process.  Every team member is more than a freelancer—their a business-minded professional looking to grow their own portfolio of work.  Unlike most other platforms, we don’t merely align you with good talent.  We stick with you through every step of the process to ensure excellent service and timely delivery."
+})
+
+list.push({
+  question: "WHERE ARE YOUR TEAM MEMBERS LOCATED?  ARE THEY FULL-TIME?",
+  answer: "Our team members are primarily US based, but we have talent around the world—including Canada, the United Kingdom, Austrailia, and more.  With some exceptions, our team members generally all work on full-time projects."
+})
+
+list.push({
+  question: "WHERE DO PIXOUL TEAM MEMBERS WORK?",
+  answer: "Generally speaking, nearly all Pixoul team members work remotely from a home office, shared office (co-working) space, or corporate business center."
+})
+
+list.push({
+  question: "IF IN-NETWORK RESOURCES ARE UNAVAILABLE, HOW LONG WILL IT TAKE TO FIND ANOTHER CANDIDATE?",
+  answer: "This can vary based on your exact requirements, skills, areas of expertise, or project scope.  Depending on what you need, we try to find you the best fit within a week, often by leveraging recommendations from our in-network professionals."
+})
+
+list.push({
+  question: "CAN I HIRE A FULL-TIME WORKER FROM PIXOUL AND BRING HIM/HER INTO OUR COMPANY?",
+  answer: "On a case-by-case basis, we do assist our clients in onboarding our team members in a more permanent position.  In that case, we will help facilitate the entire onboarding process."
+})
+
+list.push({
+  question: "CAN YOU HELP US SCREEN, VET, TEST, AND HIRE OUR OWN EMPLOYEES OUTSIDE OF THE PIXOUL NETWORK?",
+  answer: "Yes, absolutely.  We know great talent instinctively, and we want to help you find it—even if that means working outside our own network of team members."
+})
+
+list.push({
+  question: "WHO OWNS THE LEGAL RIGHTS TO THE WORK CREATED BY PIXOUL TEAM MEMBERS?",
+  answer: "Our simple, client-friendly contracts are always structured to ensure you maintain the legal copyright and ownership of all work, content, or pieces of intellectual property that our team members create."
+})
+
+list.push({
+  question: "HOW MUCH DOES PIXOUL COST?",
+  answer: "As 1099-contractors, Pixoul team members set their own hourly rates, commensurate with skill level and market demand.  Beyond an hourly rate, you’ll never pay additional fees to work with us."
+})
+
+list.push({
+  question: "WHY SHOULD I JOIN THE PIXOUL TEAM AS A FREELANCER?",
+  answer: "Pixoul team members are more than freelancers—they are business-minded professionals looking to grow their own company.  As a team member you expand your business, recieve weekly one-on-one business advising mentorship, gain access to resources that will help you market your services, and work for Fortune 500 companies."
+})
+
+
+export default list
--- src/faq/faq.js
+++ src/faq/faq.js
@@ -1,10 +1,20 @@
 import React from "react"
 import "./faq.scss"
 
+/* Utils */
+import Opening from "utils/opening/opening"
+import ExpansionPanel from "utils/expansion-panel/expansion-panel"
+/* Data */
+import data from "./data"
+
+
 const FAQ = () => {
 
   return(
-    <div className="faq">
+    <div className="faq gutters">
+
+      <Opening title="Frequently Asked Questions" />
+
 
     </div>
   )
