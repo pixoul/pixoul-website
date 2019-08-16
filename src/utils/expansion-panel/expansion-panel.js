3c996c23d075adb363080fd00543913f4d12ddef
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-16 17:44:24 -0500

    feature: creates starter expansion panel component

---
+++ src/utils/expansion-panel/expansion-panel.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react"
+import "./expansion-panel.scss"
+
+const ExpansionPanel = ({
+
+}) => {
+
+  return(
+    <div className="expansion-panel">
+
+    </div>
+  )
+}
+
+export default ExpansionPanel
