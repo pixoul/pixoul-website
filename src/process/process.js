6d99d316b9c69df786b29f30aa3494c1092ca0e6
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-16 11:03:58 -0500

    feature: implements kpi hero component on process page

--- src/process/process.js
+++ src/process/process.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./process.scss"
 
+/* Presentation Components */
+import KPIHero from "./kpi-hero"
+
 const Process = () => {
 
   return(
     <div className="process">
 
+      <KPIHero />
     </div>
   )
 }
