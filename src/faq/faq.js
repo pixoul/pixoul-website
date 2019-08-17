2c3dbb3ac43d0d466f8126976a2e647845ec5b74
Ryan Thompson <rjthompson@rjthompson.local>
2019-08-16 21:58:02 -0500

    feature: complete faq feature in full

--- src/faq/faq.js
+++ src/faq/faq.js
@@ -8,12 +8,30 @@ import ExpansionPanel from "utils/expansion-panel/expansion-panel"
 import data from "./data"
 
 
+const FAQHeader = ({
+  number,
+  question
+}) => (
+  <div className="faq-header">
+    <div className="faq-number"> {number} </div>
+    <div className="faq-question">{question}</div>
+  </div>
+)
+
 const FAQ = () => {
 
   return(
     <div className="faq gutters">
 
       <Opening title="Frequently Asked Questions" />
+
+      {data.map((faq, i) => (
+        <ExpansionPanel key={i} header={<FAQHeader number={i+1} question={faq.question} />}>
+          <div className="faq-answer">
+            {faq.answer}
+          </div>
+        </ExpansionPanel>
+      ))}
 
 
     </div>
