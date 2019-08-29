4e43b4f76a30f6dfecb9c9c0da941a5c6ea6ead7
Ryan Thompson <ryan@ryanthompson.io>
2019-08-29 14:22:45 -0500

    refactor: alters project util to use jss and adds mobile friendly rules

--- src/utils/project/project.js
+++ src/utils/project/project.js
@@ -1,7 +1,97 @@
 import React from "react"
-import "./project.scss"
+import injectSheet from 'react-jss'
 /* Utils */
 import Button from "utils/button/button"
+
+const styles = theme => ({
+  project: {
+    display: 'flex',
+    padding: 45
+  },
+  media: {
+    maxWidth: 500
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingLeft: 110
+  },
+  name: {
+    fontSize: '2.4375em',
+    fontWeight: 'bold',
+    color: '#3c3d41',
+    paddingBottom: 1
+  },
+  tagline: {
+    fontSize: '0.8125em',
+    fontWeight: 'bold',
+    color: '#656b6f',
+    textTransform: 'uppercase',
+    paddingBottom: 25
+  },
+  description: {
+    fontSize: '1.375em',
+    color: '#656b6f',
+    paddingBottom: 23
+  },
+  list: {
+    padding: {
+      bottom: 22
+    }
+  },
+  item: {
+    fontSize: '1.375em',
+    fontStyle: 'italic',
+    color: '#3c3d41',
+    "&:after" : {
+      content: '", "'
+    },
+    "&:last-child:after" : {
+      content: '""'
+    }
+  },
+  title: {
+    fontSize: '1.375em',
+    color: '#656b6f'
+  },
+  '@media (max-width: 768px)': {
+    project: {
+      flexDirection: 'column',
+      padding: 30
+    },
+    media: {
+      maxWidth: '100%'
+    },
+    content: {
+      padding: {
+        left: 0
+      }
+    },
+    name: {
+      fontSize: '2.1875em',
+      padding: {
+        top: 45
+      }
+    },
+    tagline: {
+      fontSize: '0.75em',
+      padding: {
+        top: 3,
+        bottom: 30
+      }
+    },
+    description: {
+      fontSize: '1em'
+    },
+    item: {
+      fontSize: '1em'
+    },
+    title: {
+      fontSize: '1em'
+    }
+  }
+})
 
 const Project = ({
   media,
@@ -9,30 +99,31 @@ const Project = ({
   tagline,
   description,
   services = ['N/A'],
-  tools = ['N/A']
+  tools = ['N/A'],
+  classes
 }) => (
-  <div className="project">
-    <div className="project-media">
-      <img src={media} alt={media} className="project-image" />
+  <div className={classes.project}>
+    <div>
+      <img src={media} alt={media} className={classes.media} />
     </div>
-    <div className="project-content">
-      <span className="project-name">{name}</span>
-      <span className="project-tagline">{tagline}</span>
-      <span className="project-description">{description}</span>
-      <div className="project-list">
-        <span className="list-title">Services: </span>
+    <div className={classes.content}>
+      <span className={classes.name}>{name}</span>
+      <span className={classes.tagline}>{tagline}</span>
+      <span className={classes.description}>{description}</span>
+      <div className={classes.list}>
+        <span className={classes.title}>Services: </span>
         {services.map((service, i) => (
-          <span className="list-item" key={i}>{service}</span>
+          <span className={classes.item} key={i}>{service}</span>
         ))}
       </div>
-      <div className="project-list">
-        <span className="list-title">Tools: </span>
+      <div className={classes.list}>
+        <span className={classes.title}>Tools: </span>
         {tools.map((tool, i) => (
-          <span className="list-item" key={i}>{tool}</span>
+          <span className={classes.item} key={i}>{tool}</span>
         ))}
       </div>
     </div>
   </div>
 )
 
-export default Project
+export default injectSheet(styles)(Project)
