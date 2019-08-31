8b29c3d16dc30449a2667da9d1cc4b7750dd8745
Ryan Thompson <ryan@ryanthompson.io>
2019-08-30 21:13:21 -0500

    refactor: makes form and input utils responsive

--- src/utils/form/form.js
+++ src/utils/form/form.js
@@ -7,22 +7,49 @@ import { isValidInput, getRule, applyMask } from './validate'
 
 const styles = theme => ({
   form: {
-
+    display: 'flex',
+    flexDirection: 'column',
+    padding: {
+      left: 45,
+      right: 45
+    }
   },
   fields: {
-
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
   },
   action: {
     display: 'flex',
     justifyContent: 'flex-end',
     padding: {
       top: 36,
+      right: 18,
       bottom: 67
     }
   },
   button: {
     margin: {
       left: 45
+    }
+  },
+  '@media (max-width: 768px)': {
+    form: {
+      padding: {
+        left: 0,
+        right: 0
+      }
+    },
+    action: {
+      justifyContent: 'center',
+      padding: {
+        right: 0
+      }
+    },
+    button: {
+      margin: {
+        left: 0
+      }
     }
   }
 })
