c51c624ac7c7bc696f5d8a0fd6e1c7134f026052
Ryan Thompson <ryan@ryanthompson.io>
2019-08-31 22:39:56 -0500

    refactor: alters project util to use tyopgraphy util

--- src/utils/project/project.js
+++ src/utils/project/project.js
@@ -2,11 +2,11 @@ import React from "react"
 import injectSheet from 'react-jss'
 /* Utils */
 import Button from "utils/button/button"
+import Typography from "utils/typography/typography"
 
 const styles = theme => ({
   project: {
     display: 'flex',
-    padding: 45
   },
   media: {
     maxWidth: 500
@@ -15,33 +15,9 @@ const styles = theme => ({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    paddingLeft: 110
-  },
-  name: {
-    fontSize: '2.4375em',
-    fontWeight: 'bold',
-    color: '#3c3d41',
-    paddingBottom: 1
-  },
-  tagline: {
-    fontSize: '0.8125em',
-    fontWeight: 'bold',
-    color: '#656b6f',
-    textTransform: 'uppercase',
-    paddingBottom: 25
-  },
-  description: {
-    fontSize: '1.375em',
-    color: '#656b6f',
-    paddingBottom: 23
-  },
-  list: {
-    padding: {
-      bottom: 22
-    }
+    paddingLeft: 80
   },
   item: {
-    fontSize: '1.375em',
     fontStyle: 'italic',
     color: '#3c3d41',
     "&:after" : {
@@ -50,10 +26,6 @@ const styles = theme => ({
     "&:last-child:after" : {
       content: '""'
     }
-  },
-  title: {
-    fontSize: '1.375em',
-    color: '#656b6f'
   },
   '@media (max-width: 768px)': {
     project: {
@@ -67,28 +39,6 @@ const styles = theme => ({
       padding: {
         left: 0
       }
-    },
-    name: {
-      fontSize: '2.1875em',
-      padding: {
-        top: 45
-      }
-    },
-    tagline: {
-      fontSize: '0.75em',
-      padding: {
-        top: 3,
-        bottom: 30
-      }
-    },
-    description: {
-      fontSize: '1em'
-    },
-    item: {
-      fontSize: '1em'
-    },
-    title: {
-      fontSize: '1em'
     }
   }
 })
@@ -107,21 +57,15 @@ const Project = ({
       <img src={media} alt={media} className={classes.media} />
     </div>
     <div className={classes.content}>
-      <span className={classes.name}>{name}</span>
-      <span className={classes.tagline}>{tagline}</span>
-      <span className={classes.description}>{description}</span>
-      <div className={classes.list}>
-        <span className={classes.title}>Services: </span>
-        {services.map((service, i) => (
-          <span className={classes.item} key={i}>{service}</span>
-        ))}
-      </div>
-      <div className={classes.list}>
-        <span className={classes.title}>Tools: </span>
-        {tools.map((tool, i) => (
-          <span className={classes.item} key={i}>{tool}</span>
-        ))}
-      </div>
+      <Typography variant="header1">{name}</Typography>
+      <Typography variant="subtitle2">{tagline}</Typography>
+      <Typography variant="body2">{description}</Typography>
+      <Typography variant="body2">
+        Services: {services.map((service, i) => (<span className={classes.item} key={i}>{service}</span>))}
+      </Typography>
+      <Typography variant="body2">
+        Tools: {tools.map((tool, i) => (<span className={classes.item} key={i}>{tool}</span>))}
+      </Typography>
     </div>
   </div>
 )
