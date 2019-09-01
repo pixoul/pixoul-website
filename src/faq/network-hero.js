d6f75dff60a0a5a63bd9c15f1fc82d3072688310
Ryan Thompson <rjthompson@rjthompson.local>
2019-09-01 00:00:50 -0500

    fix: cleans up faq network hero and padding for title

--- src/faq/network-hero.js
+++ src/faq/network-hero.js
@@ -3,16 +3,14 @@ import React from "react"
 import { Link } from "react-router-dom"
 /* Util */
 import Button from "utils/button/button"
-import Opening from "utils/opening/opening"
+import Typography from "utils/typography/typography"
 
 const NetworkHero = () => (
   <div className="network-hero">
-
-    <Opening
-      title="Tap Into Our Network."
-      subtitle="Scaling and managing the best on demand talent has never been faster or easier."
-    />
-
+    <div className="hero-header">
+      <Typography variant="header1" align="center">Tap Into Our Network.</Typography>
+      <Typography variant="subtitle1" align="center">Scaling and managing the best on demand talent has never been faster or easier.</Typography>
+    </div>
     <Button component={Link} to="/contact">Get Started</Button>
 
   </div>
