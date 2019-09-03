28a0c199421693ce0061c41298f06c9d49b0738b
Ryan Thompson <rjthompson@rjthompson.local>
2019-09-03 12:45:09 -0500

    refactor: alters header component to use jss instead of sass and corrects spacing and position on mobile

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,38 +1,98 @@
 import React, { useState, useEffect } from 'react';
-import Headroom from "react-headroom"
+/* Third-Party */
 import { NavLink, Link } from "react-router-dom";
-import cn from "classnames"
-import "./header.scss"
-
+import Headroom from "react-headroom"
+import injectSheet from 'react-jss'
+/* Utils */
 import Typography from "utils/typography/typography"
 import Button from "utils/button/button"
+/* Images */
 import logo from "./images/logo.svg"
 
-const Header = () => (
-    <Headroom>
-      <div className="header-container shadow">
-        <div className="header-links">
-          <NavLink to="/home" activeClassName="active-link">
+const styles = theme => ({
+  headroom: {
+    "& .headroom": {
+      zIndex: '100 !important',
+      transition: 'transform 600ms ease-in-out !important'
+    }
+  },
+  header: {
+    position: 'relative',
+    height: 100,
+    backgroundColor: theme.color.white,
+    padding: {
+      top: 0,
+      right: 150,
+      bottom: 0,
+      left: 150
+    },
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    boxShadow: '0 4px 4px -4px rgba(0, 0, 0, 0.8)'
+  },
+  links: {
+    margin: 0,
+    padding: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    "& a": {
+      textDecoration: 'none',
+      marginRight: 40
+    }
+  },
+  activeLink: {
+    fontWeight: 'bold'
+  },
+  "@media (max-width: 768px)": {
+    header: {
+      justifyContent: 'center',
+      padding: {
+        top: 0,
+        right: 30,
+        bottom: 0,
+        left: 30
+      }
+    },
+    links: {
+      "& a": {
+        marginRight: 20
+      }
+    }
+  }
+})
+
+const Header = ({
+  classes
+}) => (
+    <Headroom className={classes.headroom}>
+      <div className={classes.header}>
+        <div className={classes.links}>
+          <NavLink to="/home" activeClassName={classes.activeLink}>
             <img src={logo} alt={logo} draggable="false" />
           </NavLink>
-          <Typography variant="body2" color="primary" component={NavLink} to={'/about'} activeClassName="active-link">
+          <Typography variant="body2" color="primary" component={NavLink} to={'/about'} activeClassName={classes.activeLink}>
               About
           </Typography>
-          <Typography variant="body2" color="primary" component={NavLink}to={'/talent' }activeClassName="active-link">
+          <Typography variant="body2" color="primary" component={NavLink}to={'/talent' } activeClassName={classes.activeLink}>
               Talent
           </Typography>
-          <Typography variant="body2" color="primary" component={NavLink} to={'/process'} activeClassName="active-link">
+          <Typography variant="body2" color="primary" component={NavLink} to={'/process'} activeClassName={classes.activeLink}>
               Process
           </Typography>
-          <Typography variant="body2" color="primary" component={NavLink} to={'/faq'} activeClassName="active-link">
+          <Typography variant="body2" color="primary" component={NavLink} to={'/faq'} activeClassName={classes.activeLink}>
               FAQ
           </Typography>
         </div>
-        <div className="header-action">
-          <Button component={Link} to="/contact" outline theme="secondary">Log In</Button>
-        </div>
+
       </div>
   </Headroom>
 )
 
-export default Header
+// This goes beneath the header links once its ready to be enabled
+// <div className="header-action">
+//   <Button component={Link} to="/contact" outline theme="secondary">Log In</Button>
+// </div>
+
+export default injectSheet(styles)(Header)
