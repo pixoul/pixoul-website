cdb43f74ee65dbd479944e76a31171ebdf1b5918
Ryan Thompson <rjthompson@rjthompson.local>
2019-09-03 19:26:09 -0500

    refactor: updates all home page items to use animate component

--- src/home/top-capabilities.js
+++ src/home/top-capabilities.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom"
 import Typography from "utils/typography/typography"
 import Feature from "utils/feature/feature"
 import Button from "utils/button/button"
+import { Animate } from "utils/animate/animate"
 /* Icons */
 import DevelopmentIcon from "utils/icons/development"
 import DesignIcon from "utils/icons/design"
@@ -16,45 +17,57 @@ const TopCapabilities = () => {
   return(
     <div className="top-capabilities gutters">
         <div className="capabilities-header">
-          <Typography variant="header1" align="center">Our Top Capabilities.</Typography>
-          <Typography variant="subtitle1" align="center">Hire career-focused team members who <br /> will grow and support your initiatives.</Typography>
+          <Animate delay={100}>
+            <Typography variant="header1" align="center">Our Top Capabilities.</Typography>
+          </Animate>
+          <Animate delay={200}>
+            <Typography variant="subtitle1" align="center">Hire career-focused team members who <br /> will grow and support your initiatives.</Typography>
+          </Animate>
         </div>
 
         <Container>
           <Row>
             <Col sm={12} md={4}>
-              <Feature
-                position="top"
-                align="center"
-                title="Development"
-                icon={<DevelopmentIcon color="#276cf2" circular />}
-                description="Front-end, back-end, QA testing, and database admin."
-              />
+              <Animate delay={300}>
+                <Feature
+                  position="top"
+                  align="center"
+                  title="Development"
+                  icon={<DevelopmentIcon color="#276cf2" circular />}
+                  description="Front-end, back-end, QA testing, and database admin."
+                />
+              </Animate>
             </Col>
             <Col sm={12} md={4}>
-              <Feature
-                position="top"
-                align="center"
-                title="UX/UI DESIGN"
-                icon={<DesignIcon color="#276cf2" circular />}
-                description="Research, branding, design, animation, and product demos."
-              />
+              <Animate delay={400}>
+                <Feature
+                  position="top"
+                  align="center"
+                  title="UX/UI DESIGN"
+                  icon={<DesignIcon color="#276cf2" circular />}
+                  description="Research, branding, design, animation, and product demos."
+                />
+              </Animate>
             </Col>
             <Col sm={12} md={4}>
-              <Feature
-                position="top"
-                align="center"
-                title="PROJECT MANAGEMENT"
-                icon={<PrintIcon color="#276cf2" circular />}
-                description="Agile and waterfall methodologies, design thinking, and more."
-              />
+              <Animate delay={500}>
+                <Feature
+                  position="top"
+                  align="center"
+                  title="PROJECT MANAGEMENT"
+                  icon={<PrintIcon color="#276cf2" circular />}
+                  description="Agile and waterfall methodologies, design thinking, and more."
+                />
+              </Animate>
             </Col>
           </Row>
         </Container>
 
-        <div className="capabilities-button">
-          <Button component={Link} to="/process">Learn More</Button>
-        </div>
+        <Animate delay={300}>
+          <div className="capabilities-button">
+            <Button component={Link} to="/process">Learn More</Button>
+          </div>
+        </Animate>
     </div>
   )
 }
