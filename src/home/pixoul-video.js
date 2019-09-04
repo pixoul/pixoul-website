f19bc12d35abbc04105632bbacbe2e1753981ffb
Ryan Thompson <ryan@ryanthompson.io>
2019-09-03 19:26:09 -0500

    refactor: updates all home page items to use animate component

--- src/home/pixoul-video.js
+++ src/home/pixoul-video.js
@@ -2,18 +2,27 @@ import React from "react"
 /* Utils */
 import VideoPlayer from "utils/video-player/video-player"
 import Typography from "utils/typography/typography"
+import { Animate } from "utils/animate/animate"
 
 const PixoulVideo = () => {
   return(
     <div className="pixoul-video gutters">
       <div className="video-content">
-        <Typography variant="header1" color="white">Top Designers, Developers, and Project Managers.</Typography>
-        <Typography variant="body1" color="white"> Pixoul matches talent for industry-leading organizations, from startups to the Fortune 500.</Typography>
-        <Typography variant="body1" color="white">Our remote team members are more than freelancers—they’re career-focused individuals who invest their reputation in every project.</Typography>
+        <Animate delay={100}>
+          <Typography variant="header1" color="white">Top Designers, Developers, and Project Managers.</Typography>
+        </Animate>
+        <Animate delay={200}>
+          <Typography variant="body1" color="white"> Pixoul matches talent for industry-leading organizations, from startups to the Fortune 500.</Typography>
+        </Animate>
+        <Animate delay={300}>
+          <Typography variant="body1" color="white">Our remote team members are more than freelancers—they’re career-focused individuals who invest their reputation in every project.</Typography>
+        </Animate>
       </div>
-      <div className="video-area">
-        <VideoPlayer />
-      </div>
+      <Animate delay={300}>
+        <div className="video-area">
+          <VideoPlayer />
+        </div>
+      </Animate>
     </div>
   )
 }
