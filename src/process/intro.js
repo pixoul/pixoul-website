279995e7c41ae315335cdf2264b38e728303bb06
Ryan Thompson <rjthompson@rjthompson.local>
2019-09-05 16:15:17 -0500

    fix: updates home and process page to use video player util properly and cleans up styling for desktop

--- src/process/intro.js
+++ src/process/intro.js
@@ -4,54 +4,57 @@ import { Container, Row, Col } from 'react-grid-system'
 /* Utils */
 import Typography from "utils/typography/typography"
 import Feature from "utils/feature/feature"
+import VideoPlayer from "utils/video-player/video-player"
 /* Images */
 import play from "./images/play.svg"
+import bg from "./images/bg.png"
 
 const Intro = () => (
-  <div className="intro">
+  <VideoPlayer poster={bg} overlay autoplay loop muted>
+    <div className="intro">
+        <div className="intro-header">
+          <Typography variant="header1" color="white" align="center">How We Work.</Typography>
+          <Typography variant="subtitle1" color="white" align="center">We make is easy to find dedicated IT professionals (in days, not months).</Typography>
+        </div>
 
-    <div className="intro-header">
-      <Typography variant="header1" color="white" align="center">How We Work.</Typography>
-      <Typography variant="subtitle1" color="white" align="center">We make is easy to find dedicated IT professionals (in days, not months).</Typography>
+        <Container className="intro-features">
+          <Row>
+            <Col sm={12} md={4}>
+              <Feature
+                shrink
+                theme="light"
+                position="top"
+                align="center"
+                title="DISCUSS YOUR GOALS"
+                description="Work with a dedicated account manager."
+              />
+            </Col>
+
+            <Col sm={12} md={4}>
+              <Feature
+                shrink
+                theme="light"
+                position="top"
+                align="center"
+                title="WHITE GLOVE SELECTION"
+                description="Get matched with the right team member for your needs."
+              />
+            </Col>
+
+            <Col sm={12} md={4}>
+              <Feature
+                shrink
+                theme="light"
+                position="top"
+                align="center"
+                title="COLLABORATE"
+                description="Use our online platform to manage your new team."
+              />
+            </Col>
+          </Row>
+        </Container>
     </div>
-
-    <Container className="intro-features">
-      <Row>
-        <Col sm={12} md={4}>
-          <Feature
-            shrink
-            theme="light"
-            position="top"
-            align="center"
-            title="DISCUSS YOUR GOALS"
-            description="Work with a dedicated account manager."
-          />
-        </Col>
-
-        <Col sm={12} md={4}>
-          <Feature
-            shrink
-            theme="light"
-            position="top"
-            align="center"
-            title="WHITE GLOVE SELECTION"
-            description="Get matched with the right team member for your needs."
-          />
-        </Col>
-
-        <Col sm={12} md={4}>
-          <Feature
-            shrink
-            theme="light"
-            position="top"
-            align="center"
-            title="COLLABORATE"
-            description="Use our online platform to manage your new team."
-          />
-        </Col>
-      </Row>
-    </Container>
-  </div>
+  </VideoPlayer>
 )
 
 // This is the play button for the video background
