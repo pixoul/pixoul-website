4be6ce2da6a3904c594e809170000b63580c35d0
Ryan Thompson <ryan@ryanthompson.io>
2019-09-05 16:15:17 -0500

    fix: updates home and process page to use video player util properly and cleans up styling for desktop

--- src/home/pixoul-video.js
+++ src/home/pixoul-video.js
@@ -3,21 +3,16 @@ import React from "react"
 import VideoPlayer from "utils/video-player/video-player"
 import Typography from "utils/typography/typography"
 
-const PixoulVideo = () => {
-  return(
-    <div className="pixoul-video gutters">
+const PixoulVideo = () => (
+  <div className="pixoul-video gutters">
       <div className="video-content">
         <Typography variant="header1" color="white">Top Designers, Developers, and Project Managers.</Typography>
         <Typography variant="body1" color="white"> Pixoul matches talent for industry-leading organizations, from startups to the Fortune 500.</Typography>
         <Typography variant="body1" color="white">Our remote team members are more than freelancers—they’re career-focused individuals who invest their reputation in every project.</Typography>
       </div>
 
-      <div className="video-area">
-        <VideoPlayer />
-      </div>
-
-    </div>
-  )
-}
+      <VideoPlayer autoplay controls />
+  </div>
+)
 
 export default PixoulVideo
