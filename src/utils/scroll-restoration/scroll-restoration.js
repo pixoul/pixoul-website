4e693aa43c9960c7cf140275821917b7f9868ee4
Ryan Thompson <ryan@ryanthompson.io>
2019-09-06 13:13:45 -0500

    feature: adds scroll restoration to app to bring to top

---
+++ src/utils/scroll-restoration/scroll-restoration.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react"
+import { withRouter } from "react-router-dom"
+
+const ScrollRestoration = ({
+  children,
+  location: {
+    pathname
+  }
+}) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return children || null;
+}
+
+export default withRouter(ScrollRestoration);
