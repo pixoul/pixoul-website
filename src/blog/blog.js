c34d40b4e7606d4889aa18fb839ddf453a695b9b
Ryan Thompson <rthompson@lja.com>
2020-02-10 12:15:44 -0600

    fix: places blog init into useEffect

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./blog.scss"
 /* Presentational Components */
-function blogurl()
-{
-var s = document.createElement("script");
-s.type = "text/javascript";
-s.src = "https://dropinblog.com/js/embed.js";
-document.getElementsByTagName('head')[0].appendChild(s);
-}
+
 export default function Blog() {
-  blogurl();
+
+  const blogurl = () => {
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "https://dropinblog.com/js/embed.js";
+    document.getElementsByTagName('head')[0].appendChild(s);
+  }
+
+  useEffect(() => {
+    blogurl();
+  })
   return(
     <div className="blog">
-<div id="dib-posts"></div>
+      <div id="dib-posts"></div>
     </div>
   )
 }
