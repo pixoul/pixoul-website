921ab000191fcb135b6a9a1f55d59921ab4d3c18
Ryan Thompson <rthompson@lja.com>
2020-02-14 00:01:38 -0600

    cleanup: scripts cleanup for index and blog.js

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -2,20 +2,26 @@ import React, { useEffect } from "react";
 import "./blog.scss"
 /* Presentational Components */
 
+window.dib_id = '3IVC98TTSSRY9CULPMAU';
 export default function Blog() {
 
   const addBlogScript = () => {
-    var s = document.createElement("script");
-    s.id = 'blogScript';
-    s.type = "text/javascript";
-    s.src = "https://dropinblog.com/js/embed.js";
-    document.getElementsByTagName('body')[0].appendChild(s);
+    try {
+      var s = document.createElement("script");
+      s.id = 'blogScript';
+      s.type = "text/javascript";
+      s.src = "https://dropinblog.com/js/embed.js";
+      document.getElementsByTagName('body')[0].appendChild(s);
+    } catch(err){
+      console.error('failed to add blog properly.');
+    }
   }
 
   const removeBlogScript = () => {
     try {
-      const list = document.querySelectorAll("script[src^='https://dropinblog.com']");
-      for(const item of list){
+
+      var list = document.querySelectorAll("script[src^='https://dropinblog.com']");
+      for(var item of list){
         document.getElementsByTagName('body')[0].removeChild(item);
       }
     } catch(err){
